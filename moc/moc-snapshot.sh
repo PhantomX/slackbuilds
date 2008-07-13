abf1db724d098a7aa381d3c319c2f4477ed4b33c
Phantom X <megaphantomx@bol.com.br>
2008-07-13 03:57:47 -0300

    Try to fix broken refs...

---
+++ moc/moc-snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+module=moc
+snaproot="svn://daper.net/${module}/trunk"
+
+tmp=$(mktemp -d)
+
+trap cleanup EXIT
+cleanup() {
+  set +e
+  [ -z "${tmp}" -o ! -d "${tmp}" ] || rm -rf "${tmp}"
+}
+
+unset CDPATH
+pwd=$(pwd)
+snap=${snap:-$(date +%Y%m%d)}
+
+pushd "${tmp}"
+  svn checkout -r {$snap} ${snaproot}/ ${module}-${snap}
+  pushd ${module}-${snap}
+    find . -type d -name .svn -print0 | xargs -0r rm -rf
+  popd
+  tar -jcf "${pwd}"/${module}-${snap}.tar.bz2 ${module}-${snap}
+popd >/dev/null

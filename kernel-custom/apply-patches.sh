9862714c699b8f67d9f546e33520a328276524ad
Phantom X <megaphantomx@bol.com.br>
2008-08-10 03:44:57 -0300

    fuse, kernel 2.6.26, kmod-vmware, libcanberra, lzma, opentyrian, udev, xmms, youtube-dl
    Now that vmware modules is working, moved kernel 2.6.26 to master and dropped kernel 2.6.25.
    exmap module and svgalib_helper not working yet.

--- kernel-custom/apply-patches.sh
+++ kernel-custom/apply-patches.sh
@@ -27,36 +27,45 @@ zcat ${SB_PATCHDIR}/kernel-2.6.25-layer7-2.19.patch.gz | ${PATCHCOM} || exit 1
 # builds (as used in the buildsystem).
 zcat ${SB_PATCHDIR}/linux-2.6-build-nonintconfig.patch.gz | ${PATCHCOM} || exit 1
 
-zcat ${SB_PATCHDIR}/linux-2.6-upstream-reverts.patch.gz | ${PATCHCOM} -R || exit 1
+C=$(zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | wc -l | awk '{print $1}')
+if [ "$C" -gt 10 ]; then
+  zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | ${PATCHCOM} || exit 1
+fi
 
-#zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-stable-queue.patch.gz | ${PATCHCOM} || exit 1
+
+# revert patches from upstream that conflict or that we get via other means
+C=$(zcat ${SB_PATCHDIR}/linux-2.6-upstream-reverts.patch.gz | wc -l | awk '{print $1}')
+if [ "$C" -gt 10 ]; then
+  zcat ${SB_PATCHDIR}/linux-2.6-upstream-reverts.patch.gz | ${PATCHCOM} -R || exit 1
+fi
+
+# Roland's utrace ptrace replacement.
+zcat ${SB_PATCHDIR}/linux-2.6-ptrace-cleanup.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-tracehook.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-utrace.patch.gz | ${PATCHCOM} || exit 1
 
 # enable sysrq-c on all kernels, not only kexec
 zcat ${SB_PATCHDIR}/linux-2.6-sysrq-c.patch.gz | ${PATCHCOM} || exit 1
 
 # Architecture patches
-# x86(-64)
-# Delay longer during boot on x86 while waiting for secondary processors
-zcat ${SB_PATCHDIR}/linux-2.6-smp-boot-delay.patch.gz | ${PATCHCOM} || exit 1
-# don't map or use disabled x86 vdso
-zcat ${SB_PATCHDIR}/linux-2.6-x86-dont-map-vdso-when-disabled.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-dont-use-disabled-vdso-for-signals.patch.gz | ${PATCHCOM} || exit 1
 # dump *PIC state at boot with apic=debug
 zcat ${SB_PATCHDIR}/linux-2.6-x86-apic-dump-all-regs-v3.patch.gz | ${PATCHCOM} || exit 1
-# fix 64-bit resource on 32-bit kernels
-zcat ${SB_PATCHDIR}/linux-2.6-x86-mm-ioremap-64-bit-resource-on-32-bit-kernel.patch.gz | ${PATCHCOM} || exit 1
+
+#
+# Exec shield
+#
+zcat ${SB_PATCHDIR}/linux-2.6-execshield.patch.gz | ${PATCHCOM} || exit 1
 
 #
 # bugfixes to drivers and filesystems
 #
+
 # USB
 zcat ${SB_PATCHDIR}/linux-2.6-usb-ehci-hcd-respect-nousb.patch.gz | ${PATCHCOM} || exit 1
 zcat ${SB_PATCHDIR}/linux-2.6-usb-storage-nikond80-quirk.patch.gz | ${PATCHCOM} || exit 1
 
 # ACPI
-# acpi has a bug in the sizeof function causing thermal panics (from 2.6.26)
-zcat ${SB_PATCHDIR}/linux-2.6-acpi-fix-sizeof.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-acpi-fix-error-with-external-methods.patch.gz | ${PATCHCOM} || exit 1
 
 # Various low-impact patches to aid debugging.
 zcat ${SB_PATCHDIR}/linux-2.6-debug-sizeof-structs.patch.gz | ${PATCHCOM} || exit 1
@@ -66,11 +75,6 @@ zcat ${SB_PATCHDIR}/linux-2.6-debug-spinlock-taint.patch.gz | ${PATCHCOM} || exi
 #zcat ${SB_PATCHDIR}/linux-2.6-debug-no-quiet.patch.gz | ${PATCHCOM} || exit 1
 # try to find out what is breaking acpi-cpufreq
 zcat ${SB_PATCHDIR}/linux-2.6-debug-vm-would-have-oomkilled.patch.gz | ${PATCHCOM} || exit 1
-
-#
-# Make /dev/mem a need-to-know function
-#
-zcat ${SB_PATCHDIR}/linux-2.6-devmem.patch.gz | ${PATCHCOM} || exit 1
 
 #
 # PCI
@@ -88,15 +92,10 @@ zcat ${SB_PATCHDIR}/linux-2.6-scsi-cpqarray-set-master.patch.gz | ${PATCHCOM} ||
 
 # ALSA
 #
-zcat ${SB_PATCHDIR}/linux-2.6-alsa-kill-annoying-messages.patch.gz | ${PATCHCOM} || exit 1
 
 # Filesystem patches.
 # Squashfs
 zcat ${SB_PATCHDIR}/linux-2.6-squashfs.patch.gz | ${PATCHCOM} || exit 1
-# fix rsync inability to write to vfat partitions
-zcat ${SB_PATCHDIR}/linux-2.6-fs-fat-cleanup-code.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-fs-fat-fix-setattr.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-fs-fat-relax-permission-check-of-fat_setattr.patch.gz | ${PATCHCOM} || exit 1
 
 # Networking
 # Disable easy to trigger printk's.
@@ -111,29 +110,22 @@ zcat ${SB_PATCHDIR}/linux-2.6-serial-460800.patch.gz | ${PATCHCOM} || exit 1
 # Silence some useless messages that still get printed with 'quiet'
 zcat ${SB_PATCHDIR}/linux-2.6-silence-noise.patch.gz | ${PATCHCOM} || exit 1
 
-# add sysrq-l (show all cpus backtrace)
-zcat ${SB_PATCHDIR}/linux-2.6-sysrq-add-show-backtrace-on-all-cpus-function.patch.gz | ${PATCHCOM} || exit 1
+# Changes to upstream defaults.
+# Use UTF-8 by default on VFAT.
+zcat ${SB_PATCHDIR}/linux-2.6-defaults-fat-utf8.patch.gz | ${PATCHCOM} || exit 1
 
 # libata
-# fix hangs on undock
-zcat ${SB_PATCHDIR}/linux-2.6-libata-acpi-hotplug-fixups.patch.gz | ${PATCHCOM} || exit 1
-# fix problems with some old/broken CF hardware
-zcat ${SB_PATCHDIR}/linux-2.6-libata-be-a-bit-more-slack-about-early-devices.patch.gz | ${PATCHCOM} || exit 1
+# fix it821x
+zcat ${SB_PATCHDIR}/linux-2.6-libata-pata_it821x-driver-updates-and-reworking.patch.gz | ${PATCHCOM} || exit 1
 # Make Eee disk faster.
 zcat ${SB_PATCHDIR}/linux-2.6-sata-eeepc-faster.patch.gz | ${PATCHCOM} || exit 1
-# fix dock/undock on docking stations that have a bay
-zcat ${SB_PATCHDIR}/linux-2.6-libata-acpi-handle-bay-devices-in-dock-stations.patch.gz | ${PATCHCOM} || exit 1
-# fix ahci / ich6m conflict
-zcat ${SB_PATCHDIR}/linux-2.6-libata-ata_piix-dont-attach-to-ich6m-in-ahci-mode.patch.gz | ${PATCHCOM} || exit 1
-# fix calling sleeping function in irq context (#451896, #454954)
-zcat ${SB_PATCHDIR}/linux-2.6-libata-acpi-fix-invalid-context-acpi.patch.gz | ${PATCHCOM} || exit 1
 
 #
 # VM related fixes.
 #
 
 # wireless patches headed for 2.6.26
-zcat ${SB_PATCHDIR}/linux-2.6-wireless.patch.gz | ${PATCHCOM} || exit 1
+#zcat ${SB_PATCHDIR}/linux-2.6-wireless.patch.gz | ${PATCHCOM} || exit 1
 # wireless patches headed for 2.6.27
 zcat ${SB_PATCHDIR}/linux-2.6-wireless-pending.patch.gz | ${PATCHCOM} || exit 1
 
@@ -141,7 +133,10 @@ zcat ${SB_PATCHDIR}/linux-2.6-wireless-pending.patch.gz | ${PATCHCOM} || exit 1
 zcat ${SB_PATCHDIR}/linux-2.6-at76.patch.gz | ${PATCHCOM} || exit 1
 
 # fixups to make current wireless patches build on this kernel
-zcat ${SB_PATCHDIR}/linux-2.6-wireless-fixups.patch.gz | ${PATCHCOM} || exit 1
+#zcat ${SB_PATCHDIR}/linux-2.6-wireless-fixups.patch.gz | ${PATCHCOM} || exit 1
+
+# fix for long-standing rt2500usb issues
+zcat ${SB_PATCHDIR}/linux-2.6-rt2500usb-fix.patch.gz | ${PATCHCOM} || exit 1
 
 # implement smarter atime updates support.
 zcat ${SB_PATCHDIR}/linux-2.6-smarter-relatime.patch.gz | ${PATCHCOM} || exit 1
@@ -155,9 +150,6 @@ zcat ${SB_PATCHDIR}/linux-2.6-default-mmf_dump_elf_headers.patch.gz | ${PATCHCOM
 # http://www.lirc.org/
 zcat ${SB_PATCHDIR}/linux-2.6-lirc.patch.gz | ${PATCHCOM} || exit 1
 
-# virtio: dont hang on shutdown
-zcat ${SB_PATCHDIR}/linux-2.6-virtio_net-free-transmit-skbs-in-a-timer.patch.gz | ${PATCHCOM} || exit 1
-
 zcat ${SB_PATCHDIR}/linux-2.6-e1000-ich9.patch.gz | ${PATCHCOM} || exit 1
 
 # atl2 driver for eeepc (and others)
@@ -166,29 +158,15 @@ zcat ${SB_PATCHDIR}/linux-2.6-netdev-atl1e.patch.gz | ${PATCHCOM} || exit 1
 
 zcat ${SB_PATCHDIR}/linux-2.6-net-tulip-interrupt.patch.gz | ${PATCHCOM} || exit 1
 
-# fix bluetooth kbd disconnect
-zcat ${SB_PATCHDIR}/linux-2.6-bluetooth-signal-userspace-for-socket-errors.patch.gz | ${PATCHCOM} || exit 1
-
 # Nouveau DRM + drm fixes
-zcat ${SB_PATCHDIR}/linux-2.6-drm-git-mm.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/nouveau-drm.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/nouveau-drm-update.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-drm-i915-modeset.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-drm-radeon-fix-oops.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-drm-radeon-fix-oops2.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-drm-modesetting-oops-fixes.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-drm-fix-master-perm.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-radeon-update.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/drm-fedora9-rollup.patch.gz | ${PATCHCOM} || exit 1
 
 # linux1394 git patches
 zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-update.patch.gz | ${PATCHCOM} || exit 1
-C=$(zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | wc -l | awk '{print $1}')
-if [ "$C" -gt 10 ]; then
-  zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | ${PATCHCOM} || exit 1
-fi
-
-# usb video
-zcat ${SB_PATCHDIR}/linux-2.6-uvcvideo.patch.gz | ${PATCHCOM} || exit 1
+#C=$(zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | wc -l | awk '{print $1}')
+#if [ "$C" -gt 10 ]; then
+  #zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | ${PATCHCOM} || exit 1
+#fi
 
 # get rid of imacfb and make efifb work everywhere it was used
 zcat ${SB_PATCHDIR}/linux-2.6-merge-efifb-imacfb.patch.gz | ${PATCHCOM} || exit 1

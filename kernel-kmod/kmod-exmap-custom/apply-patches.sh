ac61b2977e3aeb6d9ee5d8d1b68cf7b6f70d1b10
Phantom X <megaphantomx@bol.com.br>
2008-08-25 05:23:56 -0300

    cksfv, comix, devede, kmod-exmap, svgalib_helper, w32codecs, xfce4-wmdock-plugin
    Now all kernel modules is working in linu 2.6.26 :)

--- kernel-kmod/kmod-exmap-custom/apply-patches.sh
+++ kernel-kmod/kmod-exmap-custom/apply-patches.sh
@@ -6,3 +6,4 @@ zcat ${SB_PATCHDIR}/${PNAME}-0.10-no_libs_on_ldflags.patch.gz | patch -p1 -E --b
 zcat ${SB_PATCHDIR}/${PNAME}-0.10-nodebug.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${PNAME}-0.10-gmake.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${PNAME}-0.10-kernelver.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${PNAME}-0.10-newkernel.patch.gz | patch -p0 -E --backup --verbose || exit 1

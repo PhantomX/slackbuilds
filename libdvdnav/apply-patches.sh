4229938a3fb028679489ef76bffa2c484f4b5e9c
Phantom X <megaphantomx@bol.com.br>
2008-09-10 02:12:15 -0300

    brofficeorg, klavaro, libdvdnav, libdvdread, libical, nmap

--- libdvdnav/apply-patches.sh
+++
@@ -1,5 +0,0 @@
-
-SB_PATCHDIR=${CWD}/patches
-
-zcat ${SB_PATCHDIR}/${NAME}-version.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-inttypes.patch.gz | patch -p1 -E --backup --verbose || exit 1

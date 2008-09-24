c198967068b6e0ae95bff626b224244d04bdf7b6
Phantom X <megaphantomx@bol.com.br>
2008-09-24 02:52:17 -0300

    atk, brofficeorg, cairo, deluge, elinks, firefox, gtk-engines, gvfs, pango, subtitleeditor, xorg-server, xulrunner and starting gnome 2.24

--- elinks/apply-patches.sh
+++ elinks/apply-patches.sh
@@ -4,7 +4,7 @@ SB_PATCHDIR=${CWD}/patches
 # Prevent crash when HOME is unset (bug #90663).
 zcat ${SB_PATCHDIR}/${NAME}-0.11.0-ssl-noegd.patch.gz | patch -p1 -E --backup --verbose || exit 1
 # UTF-8 by default
-#zcat ${SB_PATCHDIR}/${NAME}-0.10.1-utf_8_io-default.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.10.1-utf_8_io-default.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.10.1-pkgconfig.patch.gz | patch -p1 -E --backup --verbose || exit 1
 # Make getaddrinfo call use AI_ADDRCONFIG.
 zcat ${SB_PATCHDIR}/${NAME}-0.11.0-getaddrinfo.patch.gz | patch -p1 -E --backup --verbose || exit 1
@@ -14,9 +14,7 @@ zcat ${SB_PATCHDIR}/${NAME}-0.11.0-sysname.patch.gz | patch -p1 -E --backup --ve
 zcat ${SB_PATCHDIR}/${NAME}-0.10.1-xterm.patch.gz | patch -p1 -E --backup --verbose || exit 1
 # Fix #157300 - Strange behavior on ppc64
 zcat ${SB_PATCHDIR}/${NAME}-0.11.0-union.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# Fix #194096 – elinks should support negotiate-auth
-zcat ${SB_PATCHDIR}/${NAME}-0.11.1-negotiate.patch.gz | patch -p1 -E --backup --verbose || exit 1
 # fix for open macro in new glibc 
 zcat ${SB_PATCHDIR}/${NAME}-0.11.3-macropen.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# fix for broken gzip compression for chunked pages
-zcat ${SB_PATCHDIR}/${NAME}-0.11.4rc0-chunkedgzip.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-scroll.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
--- gvfs/apply-patches.sh
+++ gvfs/apply-patches.sh
@@ -1,14 +1,5 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-0.2.2-archive-integration.patch.gz | patch -p0 -E --backup --
-zcat ${SB_PATCHDIR}/${NAME}-0.2.4-libarchive-iso.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.2.4-smb-enable-debug.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-# https://bugzilla.redhat.com/show_bug.cgi?id=450453
-zcat ${SB_PATCHDIR}/${NAME}-obexftp-fallback-icon.patch.gz | patch -p0 -E --backup --verbose || exit 1
-
-# http://bugzilla.gnome.org/show_bug.cgi?id=525779
-zcat ${SB_PATCHDIR}/${NAME}-0.2.4-trash-automount.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-zcat ${SB_PATCHDIR}/bug536614.patch.gz | patch -p2 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.99.2-archive-integration.patch.gz | patch -p0 -E --backup --
+zcat ${SB_PATCHDIR}/${NAME}-obexftp-updated-apis.patch.gz | patch -p0 -E --backup --verbose || exit 1

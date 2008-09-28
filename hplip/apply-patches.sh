f8b51bac5b0c5e001bcb709acffb7368dfaedba9
Phantom X <megaphantomx@bol.com.br>
2008-09-28 05:21:46 -0300

    cdrkit, firefox, hplip, pygtk, sdlmame, xmame, xulrunner

--- hplip/apply-patches.sh
+++ hplip/apply-patches.sh
@@ -1,22 +1,14 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}.patch.gz | patch -p0 -E --backup --verbose || exit 1
 # Fix desktop file.
 zcat ${SB_PATCHDIR}/${NAME}-desktop.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-# Avoid multilib conflict
-zcat ${SB_PATCHDIR}/${NAME}-gzip-n.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Quieten hpssd on startup.
 zcat ${SB_PATCHDIR}/${NAME}-quiet.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Low ink is a warning condition, not an error.
 zcat ${SB_PATCHDIR}/${NAME}-marker-supply.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-# Make --qt4 the default for the systray applet, so that it appears in the right place.
-[ "${SB_QT4}" = "YES" ] && \
-  zcat ${SB_PATCHDIR}/${NAME}-systray.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Link libsane-hpaio against libsane (bug #234813).
 zcat ${SB_PATCHDIR}/${NAME}-libsane.patch.gz | patch -p1 -E --backup --verbose || exit 1

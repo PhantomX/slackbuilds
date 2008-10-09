bc679b26fc60c46c507b785df631d82716451874
Phantom X <megaphantomx@bol.com.br>
2008-10-09 03:06:37 -0300

    fwbuilder, gst-plugins-bad, hal, highlight, intltool, kernel, libcanberra, libfwbuilder, libxnvctrl, nvidia-settings, opera, pdf2djvu, pstreams, pulseaudio, udev

--- hal/apply-patches.sh
+++ hal/apply-patches.sh
@@ -1,6 +1,7 @@
   
 SB_PATCHDIR=${CWD}/patches
 
+zcat ${SB_PATCHDIR}/use_udevadm_not_udevinfo.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.5.10-set-property-direct.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-cloexec.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-ck-api-change.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- kernel-custom/apply-patches.sh
+++ kernel-custom/apply-patches.sh
@@ -27,6 +27,8 @@ zcat ${SB_PATCHDIR}/kernel-2.6.25-layer7-2.19.patch.gz | ${PATCHCOM} || exit 1
 # builds (as used in the buildsystem).
 zcat ${SB_PATCHDIR}/linux-2.6-build-nonintconfig.patch.gz | ${PATCHCOM} || exit 1
 
+zcat ${SB_PATCHDIR}/linux-2.6-makefile-after_link.patch.gz | ${PATCHCOM} || exit 1
+
 C=$(zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | wc -l | awk '{print $1}')
 if [ "$C" -gt 10 ]; then
   zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | ${PATCHCOM} || exit 1
@@ -46,23 +48,24 @@ zcat ${SB_PATCHDIR}/linux-2.6-utrace.patch.gz | ${PATCHCOM} || exit 1
 # enable sysrq-c on all kernels, not only kexec
 zcat ${SB_PATCHDIR}/linux-2.6-sysrq-c.patch.gz | ${PATCHCOM} || exit 1
 
+# fix sched clock monotonicity bugs
+zcat ${SB_PATCHDIR}/linux-2.6-sched-clock-fix-nohz-interaction.patch.gz | ${PATCHCOM} || exit 1
+
 # Architecture patches
 # dump *PIC state at boot with apic=debug
 zcat ${SB_PATCHDIR}/linux-2.6-x86-apic-dump-all-regs-v3.patch.gz | ${PATCHCOM} || exit 1
-# x86 f00f bug not handled properly (#197455)
-zcat ${SB_PATCHDIR}/linux-2.6-x86-64-fix-overlap-of-modules-and-fixmap-areas.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-fdiv-bug-detection-fix.patch.gz | ${PATCHCOM} || exit 1
-# another machine needing io_delay=0xed
-zcat ${SB_PATCHDIR}/linux-2.6-x86-io-delay-add-hp-f700-quirk.patch.gz | ${PATCHCOM} || exit 1
-# oprofile / hibernation fix
-zcat ${SB_PATCHDIR}/linux-2.6-x86-fix-oprofile-and-hibernation-issues.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-32-amd-c1e-force-timer-broadcast-late.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-pat-proper-tracking-of-set_memory_uc.patch.gz | ${PATCHCOM} || exit 1
-# hpet fixes from 2.6.27
-zcat ${SB_PATCHDIR}/linux-2.6-x86-hpet-01-fix-moronic-32-64-bit-thinko.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-hpet-02-read-back-compare-register.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-hpet-03-make-minimum-reprogramming-delta-useful.patch.gz | ${PATCHCOM} || exit 1
 zcat ${SB_PATCHDIR}/linux-2.6-x86-hpet-04-workaround-sb700-bios.patch.gz | ${PATCHCOM} || exit 1
+# fix e820 reservation checking
+zcat ${SB_PATCHDIR}/linux-2.6-x86-pci-detect-end_bus_number.patch.gz | ${PATCHCOM} || exit 1
+# don't oops if there's no IRQ stack available
+#zcat ${SB_PATCHDIR}/linux-2.6-x86-check-for-null-irq-context.patch.gz | ${PATCHCOM} || exit 1
+# add config option to disable adding CPUs after boot
+zcat ${SB_PATCHDIR}/linux-2.6-x86-cpu-hotplug-allow-setting-additional-cpus.patch.gz | ${PATCHCOM} || exit 1
+
+# disable ASPM on devices that don't support it
+zcat ${SB_PATCHDIR}/linux-2.6-pci-disable-aspm-per-acpi-fadt-setting.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-pci-disable-aspm-on-pre-1.1-devices.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-pci-add-an-option-to-allow-aspm-enabled-forcibly.patch.gz | ${PATCHCOM} || exit 1
 
 #
 # Exec shield
@@ -77,8 +80,6 @@ zcat ${SB_PATCHDIR}/linux-2.6-execshield.patch.gz | ${PATCHCOM} || exit 1
 zcat ${SB_PATCHDIR}/linux-2.6-usb-ehci-hcd-respect-nousb.patch.gz | ${PATCHCOM} || exit 1
 
 # ACPI
-# obvious bug in processor driver
-zcat ${SB_PATCHDIR}/linux-2.6-acpi-processor-use-signed-int.patch.gz | ${PATCHCOM} || exit 1
 # fix cpuidle misbehavior
 zcat ${SB_PATCHDIR}/linux-2.6-cpuidle-1-do-not-use-poll_idle-unless-user-asks-for-it.patch.gz | ${PATCHCOM} || exit 1
 zcat ${SB_PATCHDIR}/linux-2.6-cpuidle-2-menu-governor-fix-wrong-usage-of-measured_us.patch.gz | ${PATCHCOM} || exit 1
@@ -112,14 +113,14 @@ zcat ${SB_PATCHDIR}/linux-2.6-scsi-cpqarray-set-master.patch.gz | ${PATCHCOM} ||
 
 # block/bio
 #
-# don't discard barrier flags
-zcat ${SB_PATCHDIR}/linux-2.6-block-submit_bh-discards-barrier-flag.patch.gz | ${PATCHCOM} || exit 1
 
 # Filesystem patches.
 # cifs
 zcat ${SB_PATCHDIR}/linux-2.6-fs-cifs-turn-off-unicode-during-session-establishment.patch.gz | ${PATCHCOM} || exit 1
 # Squashfs
 zcat ${SB_PATCHDIR}/linux-2.6-squashfs.patch.gz | ${PATCHCOM} || exit 1
+# fix CIFS plaintext passwords
+zcat ${SB_PATCHDIR}/linux-2.6-fs-cifs-fix-plaintext-authentication.patch.gz | ${PATCHCOM} || exit 1
 
 # Networking
 # Disable easy to trigger printk's.
@@ -145,6 +146,10 @@ zcat ${SB_PATCHDIR}/linux-2.6-libata-pata_it821x-driver-updates-and-reworking.pa
 zcat ${SB_PATCHDIR}/linux-2.6-sata-eeepc-faster.patch.gz | ${PATCHCOM} || exit 1
 # don't use ahci for pata_marvell adapters
 zcat ${SB_PATCHDIR}/linux-2.6-libata-pata_marvell-play-nice-with-ahci.patch.gz | ${PATCHCOM} || exit 1
+# fix drivers making wrong assumptions about what dma values mean
+zcat ${SB_PATCHDIR}/linux-2.6-libata-fix-a-large-collection-of-DMA-mode-mismatches.patch.gz | ${PATCHCOM} || exit 1
+# libata breaks lba28 rules
+zcat ${SB_PATCHDIR}/linux-2.6-libata-lba-28-48-off-by-one-in-ata.h.patch.gz | ${PATCHCOM} || exit 1
 
 #
 # VM related fixes.
@@ -169,6 +174,13 @@ fi
 # fix for long-standing rt2500usb issues
 zcat ${SB_PATCHDIR}/linux-2.6-rt2500usb-fix.patch.gz | ${PATCHCOM} || exit 1
 
+# bf4634afd8bb72936d2d56425ec792ca1bfa92a2
+zcat ${SB_PATCHDIR}/linux-2.6-wireless-rt2500pci-restoring-missing-line.patch.gz | ${PATCHCOM} || exit 1
+# e95926d05d028a6bf0ab60b21b484c3d622fdcd1
+zcat ${SB_PATCHDIR}/linux-2.6-wireless-revert-b43-add-RFKILL_STATE_HARD_BLOCKED-support.patch.gz | ${PATCHCOM} || exit 1
+# aaa1553512b9105699113ea7e2ea726f3d9d4de2
+zcat ${SB_PATCHDIR}/linux-2.6-wireless-p54-fix-regression-due-to-delete-NETDEVICES_MULTIQUEUE-option.patch.gz | ${PATCHCOM} || exit 1
+
 # implement smarter atime updates support.
 zcat ${SB_PATCHDIR}/linux-2.6-smarter-relatime.patch.gz | ${PATCHCOM} || exit 1
 
@@ -182,6 +194,7 @@ zcat ${SB_PATCHDIR}/linux-2.6-default-mmf_dump_elf_headers.patch.gz | ${PATCHCOM
 zcat ${SB_PATCHDIR}/linux-2.6-lirc.patch.gz | ${PATCHCOM} || exit 1
 
 zcat ${SB_PATCHDIR}/linux-2.6-e1000-ich9.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-netdev-e1000e-fix-drv-load-issues-amt.patch.gz | ${PATCHCOM} || exit 1
 
 # atl2 driver for eeepc (and others)
 zcat ${SB_PATCHDIR}/linux-2.6-netdev-atl2.patch.gz | ${PATCHCOM} || exit 1
@@ -203,3 +216,6 @@ zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-update.patch.gz | ${PATCHCOM} || exit
 
 # get rid of imacfb and make efifb work everywhere it was used
 zcat ${SB_PATCHDIR}/linux-2.6-merge-efifb-imacfb.patch.gz | ${PATCHCOM} || exit 1
+
+zcat ${SB_PATCHDIR}/linux-2.6-intel-msr-backport.patch.gz | ${PATCHCOM} || exit 1
+zcat ${SB_PATCHDIR}/linux-2.6-libata-sff-kill-spurious-WARN_ON-in-ata_hsm_move.patch.gz | ${PATCHCOM} || exit 1

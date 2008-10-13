617eefac8703a8c1a9c770455963570aee133f14
Phantom X <megaphantomx@bol.com.br>
2008-10-13 02:17:08 -0300

    blobandconquer, hotssh, xgalaga, xf86-input-evdev, xorg-server

--- xorg/xorg-server/apply-patches.sh
+++ xorg/xorg-server/apply-patches.sh
@@ -15,13 +15,20 @@ zcat ${SB_PATCHDIR}/xserver-1.4.99-dont-backfill-bg-none.patch.gz | patch -p1 --
 zcat ${SB_PATCHDIR}/xserver-1.5.0-exa-master-upgrade.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-bg-none-root.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-exa-master-fix-x11perf-crash.patch.gz | patch -p1 --verbose || exit 1
+zcat ${SB_PATCHDIR}/xserver-1.5.1-exa-fix-glyph-segfault.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.4.99-pic-libxf86config.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-projector-fb-size.patch.gz | patch -p1 --verbose || exit 1
-zcat ${SB_PATCHDIR}/xserver-1.5.0-comment-out-glxdri2.patch.gz | patch -p1 --verbose || exit 1
 # This really could be done prettier.
 zcat ${SB_PATCHDIR}/xserver-1.4.99-ssh-isnt-local.patch.gz | patch -p1 --verbose || exit 1
+zcat ${SB_PATCHDIR}/xserver-1.5.1-xgi.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-bad-fbdev-thats-mine.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.4.99-endian.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-force-SwitchCoreKeyboard-for-evdev.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-hide-cursor.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/xserver-1.5.0-edid-backport.patch.gz | patch -p1 --verbose || exit 1
+
+# force mode debugging on for randr 1.2 drivers
+zcat ${SB_PATCHDIR}/xserver-1.5.1-mode-debug.patch.gz | patch -p1 --verbose || exit 1
+
+zcat ${SB_PATCHDIR}/xserver-1.5.1-global-backtrace.patch.gz | patch -p1 --verbose || exit 1
+zcat ${SB_PATCHDIR}/xserver-1.5.2-mieq-backtrace.patch.gz | patch -p1 --verbose || exit 1

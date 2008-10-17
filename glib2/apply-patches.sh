96fd832078fe5b1d601efdbe35fe821bce329eb9
Phantom X <megaphantomx@bol.com.br>
2008-10-17 20:46:23 -0300

    e2fsprogs, glib2, gtk+2, libpciaccess, openssl, xf86-input-evdev

--- glib2/apply-patches.sh
+++ glib2/apply-patches.sh
@@ -4,6 +4,3 @@ SB_PATCHDIR=${CWD}/patches
 # https://bugzilla.redhat.com/show_bug.cgi?id=442835
 # http://bugzilla.gnome.org/show_bug.cgi?id=528670
 zcat ${SB_PATCHDIR}/gio-2.18-always-pass-fuse-file-uri.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-# Plug a small memory leak, will be included in 2.18.2
-zcat ${SB_PATCHDIR}/${PNAME}-2.18.1-gdesktopappinfo-memleak-fix.patch.gz | patch -p2 -E --backup --verbose || exit 1

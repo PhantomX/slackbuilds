07fc1d3376fa52888ae8ae9ff34049dd6ab7a1d6
Phantom X <megaphantomx@bol.com.br>
2008-10-22 05:47:00 -0200

    gphoto2, libdrm, libgphoto2, pango, xf86driproto, xorg-server, wireshark

--- wireshark/apply-patches.sh
+++ wireshark/apply-patches.sh
@@ -8,3 +8,4 @@ zcat ${SB_PATCHDIR}/${NAME}-nfsv41.patch.gz | patch -p1 -E --backup --verbose ||
 zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-types.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-updates.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-rpc-pdu-size.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}_pipe.patch.gz | patch -p1 -E --backup --verbose || exit 1

28cfaee0c61f398d40c2baed03f98482bfa6dc4a
Phantom X <megaphantomx@bol.com.br>
2008-10-24 23:21:44 -0200

    chromium, fftw, ftgl, glpng, gmime20, libv4l, openal, sharutils, swfdec, vbam, virtualbox and module, xdialog

--- ftgl/apply-patches.sh
+++
@@ -1,8 +0,0 @@
-
-SB_PATCHDIR=${CWD}/patches
-
-zcat ${SB_PATCHDIR}/${NAME}-2.1.2-destdir.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.1.2-Glyph-g++_41.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.1.2-pc_req.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.1.2-ttf_font.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.1.2-rpath_FTGLDemo.patch.gz | patch -p1 -E --backup --verbose || exit 1

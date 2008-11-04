460fee71d824ad3436452143cb01517fa9667097
Phantom X <megaphantomx@bol.com.br>
2008-11-04 02:10:33 -0200

    gecko-mediaplayer, gnome-mplayer, hdparm, imagemagick, ncurses, pkcs11-helper, tack, truecrypt

--- ncurses/apply-patches.sh
+++ ncurses/apply-patches.sh
@@ -1,11 +1,16 @@
 
 SB_PATCHDIR=${CWD}/patches
 
-bzcat ${CWD}/${NAME}-5.6-20080621-patch.sh.bz2 | patch -p1 --verbose || exit 1
-rm -rf tack
-for file in ${SB_PATCHDIR}/updates/${NAME}-${VERSION}-2???????.patch.gz ; do
-  zcat ${file} | patch -p1 --verbose || exit 1
-done
+if [ "${PVER}" -ne 0 ] ; then
+  bzcat ${CWD}/${PSRCARCHIVE} | patch -p1 --verbose || exit 1
+fi
+
+C=$(wc -l ${CWD}/${PSRCARCHIVES} | awk '{print $1}')
+if [ "$C" -gt 0 ]; then
+  for file in $(<${CWD}/${PSRCARCHIVES}) ; do
+    zcat ${SB_PATCHDIR}/updates/${file} | patch -p1 --verbose || exit 1
+  done
+fi
 
 zcat ${SB_PATCHDIR}/${NAME}.mkhashsize.diff.gz | patch -p1 --verbose || exit 1
 

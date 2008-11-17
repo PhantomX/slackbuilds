61e06ad0e3da2cbd55f0c72ea3b11f0b89d0f1bc
Phantom X <megaphantomx@bol.com.br>
2008-11-17 02:42:26 -0200

    clamtk, fftw, fontforge, imagemagick, memtest86+, qgtkstyle, redhat-menus, solar-backgrounds

--- id3lib/apply-patches.sh
+++ id3lib/apply-patches.sh
@@ -1,7 +1,6 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-dox.patch.gz | patch -p0 -E --backup --verbose || exit 1
 bzcat ${SB_PATCHDIR}/${NAME}-3.8.3-libtool-autofoo.patch.bz2 | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-3.8.3-io_helpers-163101.patch.gz | patch -p1 -E --backup --verbose || exit 1

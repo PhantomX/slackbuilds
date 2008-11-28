aae9c5b53f21cdc74cb2a051c8c39d26a9f0ca58
Phantom X <megaphantomx@bol.com.br>
2008-11-28 01:55:20 -0200

    dpkg, gnome-desktop, gnome-panel, gnome-menus, gnome-session, libcap, libgweather, libwnck, sdl_image

--- gnome/gnome-session/apply-patches.sh
+++ gnome/gnome-session/apply-patches.sh
@@ -1,6 +1,3 @@
 
 SB_PATCHDIR=${CWD}/patches
 
-# http://bugzilla.gnome.org/show_bug.cgi?id=536915
-zcat ${SB_PATCHDIR}/${NAME}-2.24.0-add-can-shutdown-api.patch.gz | patch -p1 -E --backup --verbose || exit 1
-

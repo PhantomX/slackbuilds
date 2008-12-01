e5bf87dc5f7dc8702d312e60021ef308a64000a4
Phantom X <megaphantomx@bol.com.br>
2008-12-01 02:36:40 -0200

    avidemux, libnetfilter_conntrack, qdvdauthor, quassel, xmoto, yabause

--- xmoto/apply-patches.sh
+++ xmoto/apply-patches.sh
@@ -2,7 +2,6 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${NAME}-0.3.4-Environment-cstlib.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.3.4-Scene-collisioninclude.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.5.0-Scene-collisioninclude.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.4.0-Environment-string.patch.gz | patch -p0 -E --backup --verbose || exit 1
 #zcat ${SB_PATCHDIR}/${NAME}-0.4.0-DrawLib-hashmap.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-ode-0.10.patch.gz | patch -p1 -E --backup --verbose || exit 1

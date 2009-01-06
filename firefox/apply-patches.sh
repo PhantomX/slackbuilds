3da0460e5f086962b27066669cdcc311727b9792
Phantom X <megaphantomx@bol.com.br>
2009-01-06 02:44:59 -0200

    blobandconquer, boost, deluge, desmume, espeak, gavl, gimp, gmerlin-avdecoder, gmerlin, libburn, libtorrent-rasterbar, mame-data, nvclock, openmovieeditor, pygobject, qbittorrent, qmc2, rkhunter, sdlmame, system-config-printer, valgrind, wine, wine_gecko

--- firefox/apply-patches.sh
+++ firefox/apply-patches.sh
@@ -4,4 +4,6 @@ SB_PATCHDIR=${CWD}/patches
 # FS#10836: fixes backgroundcolor parsing with gnome
 zcat ${SB_PATCHDIR}/mozbug421977.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
+zcat ${SB_PATCHDIR}/030-firefox_encode_spaces.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
 # Upstream patches

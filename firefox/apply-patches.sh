a7b0840c68b480ee242442ba16359472521fa4f5
Phantom X <megaphantomx@bol.com.br>
2009-02-05 03:47:39 -0200

    file, firefox, libraw1394, neverball, ogmrip, pyopenssl, xulrunner

--- firefox/apply-patches.sh
+++ firefox/apply-patches.sh
@@ -5,5 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 zcat ${SB_PATCHDIR}/mozbug421977.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
 zcat ${SB_PATCHDIR}/030-firefox_encode_spaces.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/667_typeahead-broken-v4.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
 # Upstream patches
--- xulrunner/apply-patches.sh
+++ xulrunner/apply-patches.sh
@@ -15,7 +15,12 @@ zcat ${SB_PATCHDIR}/mozilla-pkgconfig.patch.gz | patch -p1 -E --backup --verbose
 # Fix build with system hunspell (from gentoo)
 zcat ${SB_PATCHDIR}/mozilla-system-hunspell-corrections.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
+zcat ${SB_PATCHDIR}/090_unaligned.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/667_typeahead-broken-v4.patch.gz | patch -p0 -E --backup --verbose || exit 1
+
+# From Arch
+zcat ${SB_PATCHDIR}/fix-mozilla-launcher.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/nsThreadUtils.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Upstream patches
 zcat ${SB_PATCHDIR}/mozilla-ps-pdf-simplify-operators.patch.gz | patch -p1 -E --backup --verbose || exit 1

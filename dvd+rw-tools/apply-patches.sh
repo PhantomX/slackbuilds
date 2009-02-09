e2a70ca6615597954bd3c28122a2597d85b16cc4
Phantom X <megaphantomx@bol.com.br>
2009-02-09 04:27:31 -0200

    ack, amp, mp3blaster, stellarium

--- dvd+rw-tools/apply-patches.sh
+++ dvd+rw-tools/apply-patches.sh
@@ -2,5 +2,8 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${NAME}-7.0-man.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-7.0-wexit.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-7.0-glibc2.6.90.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-7.0-reload.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-7.0-wctomb.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-7.0-dvddl.patch.gz | patch -p0 -E --backup --verbose || exit 1

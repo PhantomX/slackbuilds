35703d46adb072f05f206f415a82260709079610
Phantom X <megaphantomx@bol.com.br>
2009-03-04 02:38:58 -0300

    cpulimit, libewf, libsndfile, lm_sensors, mednafen, opera, perl-extutils-pkgconfig, privoxy, qt-creator, qt4, testdisk

--- qt4/apply-patches.sh
+++ qt4/apply-patches.sh
@@ -10,13 +10,11 @@ fi
 
 zcat ${SB_PATCHDIR}/${PNAME}-all-opensource-src-4.4.0-rc1-as_IN-437440.patch.gz | patch -p1 -E --verbose || exit 1
 
-# under GNOME, default to QGtkStyle if available
-# (otherwise fall back to QCleanlooksStyle)
-zcat ${SB_PATCHDIR}/${PNAME}-x11-opensource-src-4.4.0-qgtkstyle.patch.gz | patch -p1 -E --verbose || exit 1
-zcat ${SB_PATCHDIR}/${PNAME}-x11-opensource-src-4.4.3-im.patch.gz | patch -p1 -E --verbose || exit 1
+zcat ${SB_PATCHDIR}/${PNAME}-x11-opensource-src-4.5.0-rc1-ppc64.patch.gz | patch -p1 -E --verbose || exit 1
+## upstreamable bits
+# http://bugzilla.redhat.com/485677
+zcat ${SB_PATCHDIR}/${PNAME}-x11-opensource-src-4.5.0-rc1-qhostaddress.patch.gz | patch -p1 -E --verbose || exit 1
 
 # qt-copy patches
-echo "0242" >> patches/DISABLED
 echo "0250" >> patches/DISABLED
-echo "0251" >> patches/DISABLED
 test -x apply_patches && ./apply_patches

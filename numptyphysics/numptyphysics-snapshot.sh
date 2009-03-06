2d53018739ab45402723afe5013185d7128458fb
Phantom X <megaphantomx@bol.com.br>
2009-03-06 02:31:54 -0300

    mkvtoolnix, numptyphysics, numptyphysics-levels

---
+++ numptyphysics/numptyphysics-snapshot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+module=$(basename $0 -snapshot.sh)
+snaproot="https://garage.maemo.org/svn/${module}/trunk"
+
+tmp=$(mktemp -d)
+
+trap cleanup EXIT
+cleanup() {
+  set +e
+  [ -z "${tmp}" -o ! -d "${tmp}" ] || rm -rf "${tmp}"
+}
+
+unset CDPATH
+pwd=$(pwd)
+snap=${snap:-$(date +%Y%m%d)}
+
+[ "${snap}" = "$(date +%Y%m%d)" ] || SNAP_COOPTS="-r {$snap}"
+
+pushd "${tmp}"
+  svn checkout ${SNAP_COOPTS} ${snaproot} ${module}-${snap}
+  pushd ${module}-${snap}
+    find . -type d -name .svn -print0 | xargs -0r rm -rf
+  popd >/dev/null
+  tar -jcf "${pwd}"/${module}-${snap}.tar.bz2 ${module}-${snap}
+popd >/dev/null

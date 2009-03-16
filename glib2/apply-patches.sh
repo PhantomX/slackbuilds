86293a4dcafb02d8e2ce43df3d2e45acb2f0721d
Phantom X <megaphantomx@bol.com.br>
2009-03-16 01:18:22 -0300

    freetype, glib2, graphviz, gtk+2, gvfs, iasl, jhead, libburn, libisofs, lxmenu-data, lxpanel, lxsession-edit, lxshortcut. menu-cache, transmission, wine, xf86-input-evdev

--- glib2/apply-patches.sh
+++ glib2/apply-patches.sh
@@ -1,6 +1,2 @@
   
 SB_PATCHDIR=${CWD}/patches
-
-# https://bugzilla.redhat.com/show_bug.cgi?id=442835
-zcat ${SB_PATCHDIR}/gio-2.18-always-pass-fuse-file-uri.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/glib-i386-atomic.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- gtk+2/apply-patches.sh
+++ gtk+2/apply-patches.sh
@@ -6,12 +6,12 @@ SB_PATCHDIR=${CWD}/patches
 # and this patch is the workaround.  ;-)
 zcat ${SB_PATCHDIR}/gtk.pnglz.diff.gz | patch -p1 -E --backup --verbose || exit 1
 
-# Fedora patch
-zcat ${SB_PATCHDIR}/${PNAME}-2.11.1-set-invisible-char-to-bullet.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
 # Workaround adobe flash infinite loop.
 # Patch from http://bugzilla.gnome.org/show_bug.cgi?id=463773#c11
 zcat ${SB_PATCHDIR}/${PNAME}-2.12.0-flash-workaround.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# http://bugzilla.redhat.com/show_bug.cgi?id=478400
+zcat ${SB_PATCHDIR}/default_printer.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
 # Don't break inclusion of gtkclist.h, upstream bug 536767
 zcat ${SB_PATCHDIR}/${PNAME}-2.14.3-limit-gtksignal-includes.patch.gz | patch -p0 -E --backup --verbose || exit 1

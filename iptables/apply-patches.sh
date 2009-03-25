360eb196beb5738c718feb9f2ac6b87581f218c8
Phantom X <megaphantomx@bol.com.br>
2009-03-25 02:11:18 -0300

    bzr, bzrtools, dvdstyler, imagemagick, iptables, metisse, nucleo

--- iptables/apply-patches.sh
+++ iptables/apply-patches.sh
@@ -1,6 +1,4 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-1.3.8-typo_latter.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.4.1.1-cloexec.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.4.1-nf_ext_init.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.4.3-cloexec.patch.gz | patch -p1 -E --backup --verbose || exit 1

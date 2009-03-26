30cd40d5fa6766aaf74f699e136125ee55895d73
Phantom X <megaphantomx@bol.com.br>
2009-03-26 02:52:16 -0300

    filezilla, iptables, libiptcdata, libxnvctrl, lyx, mercurial, nvidia-{glx,settings}, openssl, postgresql, pyopenssl, q4wine, system-config-printer, subversion

--- openssl/apply-patches.sh
+++ openssl/apply-patches.sh
@@ -6,24 +6,24 @@ zcat ${SB_PATCHDIR}/${NAME}.soname.diff.gz | patch -p1 --backup --verbose --suff
 zcat ${SB_PATCHDIR}/${NAME}.optsx86.diff.gz | patch -p1 --backup --verbose --suffix=.orig || exit 1
 
 # Build changes
-#zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-redhat.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-0.9.8j-redhat.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8a-defaults.patch.gz | patch -p1 -E --backup --verbose || exit 1
 #zcat ${SB_PATCHDIR}/${NAME}-0.9.8a-link-krb5.patch.gz | patch -p1 -E --backup --verbose || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-soversion.patch.gz | patch -p1 -E --backup --verbose || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-0.9.8a-enginesdir.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-0.9.8j-soversion.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-0.9.8j-enginesdir.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8a-no-rpath.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8b-test-use-localhost.patch.gz | patch -p1 -E --backup --verbose || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-shlib-version.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-0.9.8j-shlib-version.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Bug fixes
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8b-aliasing-bug.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.9.8b-x509-name-cmp.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.9.8k-x509-name-cmp.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-default-paths.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-no-extssl.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Functionality changes
-#zcat ${SB_PATCHDIR}/${NAME}-0.9.7-beta6-ia64.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-ia64.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.6-x509.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.9.7-beta5-version-add-engines.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.9.8j-version-add-engines.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8a-reuse-cipher-change.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.8g-ipv6-apps.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- subversion/apply-patches.sh
+++ subversion/apply-patches.sh
@@ -2,7 +2,9 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${NAME}-0.20.1-deplibs.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.5.1-rpath.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.5.0-pie.patch.gz | patch -p1 -E --backup --verbose || exit 1
-[ "${SB_JAVA}" = "YES" ] && \
+zcat ${SB_PATCHDIR}/${NAME}-1.6.0-rpath.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.6.0-pie.patch.gz | patch -p1 -E --backup --verbose || exit 1
+if [ "${SB_JAVA}" = "YES" ] ;then
   zcat ${SB_PATCHDIR}/${NAME}-1.1.3-java.patch.gz | patch -p1 -E --backup --verbose || exit 1
+fi
+zcat ${SB_PATCHDIR}/${NAME}-1.6.0-various_fixes.patch.gz | patch -p0 -E --backup --verbose || exit 1

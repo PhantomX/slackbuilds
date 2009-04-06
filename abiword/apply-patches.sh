00d085cc9d7b4815afdbaca951629b0946ef371f
Phantom X <megaphantomx@bol.com.br>
2009-04-06 03:04:11 -0300

    abiword, epdfview, hydrogen, portmidi

--- abiword/apply-patches.sh
+++ abiword/apply-patches.sh
@@ -7,6 +7,7 @@ zcat ${SB_PATCHDIR}/${NAME}-2.6.0-boolean.patch.gz | patch -p1 -E --backup --ver
 # abiword-plugins
 ( cd ${SRCDIR2} || exit 1
   zcat ${SB_PATCHDIR}/${NAME}-plugins-2.6.0-boolean.patch.gz | patch -p1 -E --backup --verbose || exit 1
+  zcat ${SB_PATCHDIR}/${NAME}-plugins-2.6.8-rindex.patch.gz | patch -p1 -E --backup --verbose || exit 1
 ) || exit 1
 
 # abiword documentation

8f2d5842fcbd54f112464cbb5a5d2ed0aaaa1892
Phantom X <megaphantomx@bol.com.br>
2009-04-08 00:00:23 -0300

    deluge, gxine, libcddb, libX11, xf86-video-nv, xinput

--- xorg/libX11/apply-patches.sh
+++ xorg/libX11/apply-patches.sh
@@ -1,6 +1,4 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-# zcat ${SB_PATCHDIR}/${NAME}-0.99.3-datadir-locale-dir-fix.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-dont-forward-keycode-0.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.1.99.2-compose-updates.patch.gz | patch -p1 -E --backup --verbose || exit 1

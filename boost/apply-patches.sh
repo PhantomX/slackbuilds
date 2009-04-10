06601ef4b78e444491fff8acc4246a59d89b37f9
Phantom X <megaphantomx@bol.com.br>
2009-04-10 05:02:38 -0300

    asio, boost, dpkg, gmime, irssi, m4, perl-{termreadkey,text-{charwidth,wrapi18n}}, po4a, qscintilla, wireshark

--- boost/apply-patches.sh
+++ boost/apply-patches.sh
@@ -4,11 +4,8 @@ SB_PATCHDIR=${CWD}/patches
 zcat ${SB_PATCHDIR}/${NAME}-configure.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-gcc-soname.patch.gz |sed "s/__SONAME__/${SONAMEVER}/" | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-run-tests.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-gcc43.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-function_template.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-unneccessary_iostreams.patch.gz | patch -p0 -E --backup --verbose || exit 1
 
 # Gentoo patches
 zcat ${SB_PATCHDIR}/${NAME}-1.35.0-fix_mpi_installation.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.36.0-tools-build-fix.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.36.0-compiler_status-trailing_slash.gz | patch -p0 -E --backup --verbose || exit 1
--- wireshark/apply-patches.sh
+++ wireshark/apply-patches.sh
@@ -8,4 +8,6 @@ zcat ${SB_PATCHDIR}/${NAME}-nfsv41.patch.gz | patch -p1 -E --backup --verbose ||
 zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-types.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-updates.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-rpc-pdu-size.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.0.5-nfs41-backchnl-decode.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.0.6-netdump2.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}_pipe.patch.gz | patch -p1 -E --backup --verbose || exit 1

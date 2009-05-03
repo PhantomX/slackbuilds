4d7041024112e0adfe4c35b54f7078bb823c64aa
Phantom X <megaphantomx@bol.com.br>
2009-05-03 02:36:44 -0300

    anjuta, brofficeorg, cmake, elfutils, geany, libunique (former unique), gnutls, ncdu, qca, qemu

--- elfutils/apply-patches.sh
+++ elfutils/apply-patches.sh
@@ -10,6 +10,7 @@ if [ "${SB_COMPAT}" = "YES" ] ;then
   find . \( -name Makefile.in -o -name aclocal.m4 \) -print | xargs touch
   sleep 1
   find . \( -name configure -o -name config.h.in \) -print | xargs touch
+  autoconf -f || exit 1
 else
   sed -i.scanf-m -e 's/%m/%a/' src/addr2line.c tests/line2addr.c || exit 1
 fi

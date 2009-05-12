5192c0f53b1568b93f4250215b35d2c921b685b9
Phantom X <megaphantomx@bol.com.br>
2009-05-12 02:05:03 -0300

    dbus, gnome-bluetooth, gphoto2, l7-protocols, libgphoto2, psi, shadow, youtube-dl

--- libgphoto2/apply-patches.sh
+++ libgphoto2/apply-patches.sh
@@ -6,4 +6,3 @@ zcat ${SB_PATCHDIR}/${NAME}-storage.patch.gz | patch -p1 -E --backup --verbose |
 zcat ${SB_PATCHDIR}/${NAME}-ixany.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-maxentries.patch.gz | patch -p1 -E --backup --verbose || 
 zcat ${SB_PATCHDIR}/${NAME}-libusb-fix-build.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/no-mdns.patch.gz | patch -p0 -E --backup --verbose || exit 1
--- shadow/apply-patches.sh
+++ shadow/apply-patches.sh
@@ -4,12 +4,9 @@ SB_PATCHDIR=${CWD}/patches
 zcat ${SB_PATCHDIR}/${NAME}-shadowconfig-man.patch.gz | patch -p1 --verbose --backup || exit 1
 zcat ${SB_PATCHDIR}/${NAME}.login.defs.diff.gz | patch -p0 --verbose --backup || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.1.3-redhat1.patch.gz | patch -p1 --verbose --backup || exit 1
-#if [ "${SB_RH}" = "YES" ]; then
-  #zcat ${SB_PATCHDIR}/${NAME}-4.1.2-redhat2.patch.gz | patch -p1 --verbose --backup || exit 1
-#fi
+if [ "${SB_RH}" = "YES" ]; then
+  zcat ${SB_PATCHDIR}/${NAME}-4.1.3-redhat2.patch.gz | patch -p1 --verbose --backup || exit 1
+fi
 
-#zcat ${SB_PATCHDIR}/${NAME}-4.1.2-goodname.patch.gz | patch -p1 --verbose --backup || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-4.1.2-sysAccountDownhill.patch.gz | patch -p1 --verbose --backup || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-4.1.2-gmSEGV.patch.gz | patch -p1 --verbose --backup || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-4.1.2-checkName.patch.gz | patch -p1 --verbose --backup || exit 1
-#zcat ${SB_PATCHDIR}/${NAME}-4.1.2-gmNoGroup.patch.gz | patch -p1 --verbose --backup || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-4.1.3-goodname.patch.gz | patch -p1 --verbose --backup || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-4.1.3-lastlog.patch.gz | patch -p1 --verbose --backup || exit 1

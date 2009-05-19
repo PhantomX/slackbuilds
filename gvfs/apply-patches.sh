31b87cf1cdc3eb17e747a06be2d8b2d28e50daea
Phantom X <megaphantomx@bol.com.br>
2009-05-19 01:51:52 -0300

    devicekit-disks, gnome-disk-utility, gtk-engines, gucharmap, gvfs, libatasmart, libgphoto2, libsoup, ncurses and some gnome 2.26.2 apps.

--- gvfs/apply-patches.sh
+++ gvfs/apply-patches.sh
@@ -6,10 +6,10 @@ zcat ${SB_PATCHDIR}/${NAME}-0.99.2-archive-integration.patch.gz | patch -p0 -E -
 # http://bugzilla.gnome.org/show_bug.cgi?id=573826
 zcat ${SB_PATCHDIR}/${NAME}-1.1.7-gdu-computer-expose-devices.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
-# http://bugzilla.gnome.org/show_bug.cgi?id=578681
-zcat ${SB_PATCHDIR}/${NAME}-bash-completion.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# From svn:
-zcat ${SB_PATCHDIR}/${NAME}-1.2.2-ssh-auth-sock.patch.gz | patch -p0 -E --backup --verbose || exit 1
+# https://bugzilla.redhat.com/show_bug.cgi?id=499266
+zcat ${SB_PATCHDIR}/${NAME}-1.2.3-cdda-allow-query-well-formed-filenames-only.patch.gz | patch -p1 -E --backup --verbose || exit 1
+# https://bugzilla.redhat.com/show_bug.cgi?id=497631 
+zcat ${SB_PATCHDIR}/${NAME}-1.2.2-dnssd-deadlock.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Gdu volume monitor patches, from http://cgit.freedesktop.org/~david/gvfs/log/?h=gdu-volume-monitor
 #
@@ -27,3 +27,16 @@ zcat ${SB_PATCHDIR}/gdu-0006-Ignore-drives-if-all-volumes-of-the-drive-are-ignor
 # https://bugzilla.redhat.com/show_bug.cgi?id=495152
 zcat ${SB_PATCHDIR}/gdu-0008-ignore-drives-without-volumes.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/gdu-0009-never-ignore-drives-without-media.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# http://bugzilla.gnome.org/show_bug.cgi?id=536292
+zcat ${SB_PATCHDIR}/gdu-0010-show-user-mountable-fstab-entries.patch.gz | patch -p1 -E --backup --verbose || exit 1
+# http://bugzilla.gnome.org/show_bug.cgi?id=576083
+zcat ${SB_PATCHDIR}/gdu-0011-Bug-576083-pre-unmount-signals-not-being-trigger.patch.gz | patch -p1 -E --backup --verbose || exit 1
+# https://bugzilla.redhat.com/show_bug.cgi?id=495170
+zcat ${SB_PATCHDIR}/gdu-0012-use-new-gnome-disk-utility-API-to-hide-unwanted-devi.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/gdu-0013-pass-the-flush-mount-option-for-vfat.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# Only automount when media has just been inserted
+zcat ${SB_PATCHDIR}/0014-gvfs-use-device-media-detected.patch.gz | patch -p1 -E --backup --verbose || exit 1
+# https://bugzilla.redhat.com/show_bug.cgi?id=498649
+zcat ${SB_PATCHDIR}/0015-gvfs-respect-presentation-hide-for-drives.patch.gz | patch -p1 -E --backup --verbose || exit 1

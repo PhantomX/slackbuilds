bb649499bdde403cbef71f5bb33ba6fd7f26d0e2
Phantom X <megaphantomx@bol.com.br>
2009-06-01 02:55:50 -0300

    e2fsprogs-libs, gtk+2, hal, libgda, libisofs, lvm2, psi, psimedia, udev

--- gtk+2/apply-patches.sh
+++ gtk+2/apply-patches.sh
@@ -6,9 +6,9 @@ SB_PATCHDIR=${CWD}/patches
 # and this patch is the workaround.  ;-)
 zcat ${SB_PATCHDIR}/gtk.pnglz.diff.gz | patch -p1 -E --backup --verbose || exit 1
 
-# Workaround adobe flash infinite loop.
-# Patch from http://bugzilla.gnome.org/show_bug.cgi?id=463773#c11
-zcat ${SB_PATCHDIR}/${PNAME}-2.12.0-flash-workaround.patch.gz | patch -p1 -E --backup --verbose || exit 1
+# Fix for x86_64 (does not hurt the other archs but note that we will start
+# using /etc/gtk-2.0/$host instead of /etc/gtk-2.0/ now):
+zcat ${SB_PATCHDIR}/gtk.etc.host.location.diff.gz | patch -p1 --verbose || exit 1
 
 # http://bugzilla.redhat.com/show_bug.cgi?id=478400
 zcat ${SB_PATCHDIR}/default_printer.patch.gz | patch -p0 -E --backup --verbose || exit 1
--- hal/apply-patches.sh
+++ hal/apply-patches.sh
@@ -1,7 +1,14 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/use_udevadm_not_udevinfo.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.5.10-set-property-direct.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-cloexec.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-ck-api-change.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+zcat ${SB_PATCHDIR}/${NAME}-add-keys-to-buttons.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# https://bugzilla.redhat.com/show_bug.cgi?id=488177
+zcat ${SB_PATCHDIR}/${NAME}-remove-dell-killswitch.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+zcat ${SB_PATCHDIR}/${NAME}-tablet-evdev.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# http://lists.freedesktop.org/archives/hal/2009-March/013125.html
+zcat ${SB_PATCHDIR}/${NAME}-KVM-evdev.patch.gz | patch -p1 -E --backup --verbose || exit 1

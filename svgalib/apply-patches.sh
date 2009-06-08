2d7b659196cad9c7f752948036c7d1fa80c8cb74
Phantom X <megaphantomx@bol.com.br>
2009-06-08 03:30:39 -0300

    cfv, cython, gecko-mediaplayer, gnome-mplayer, lshw, mkvtoolnix, p7zip, torrentinfo

--- svgalib/apply-patches.sh
+++ svgalib/apply-patches.sh
@@ -7,3 +7,4 @@ zcat ${SB_PATCHDIR}/${NAME}-1.9.21-demos.patch.gz | patch -p1 -E --backup --verb
 zcat ${SB_PATCHDIR}/${NAME}-1.9.21-cfg.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.9.25-kernel-2.6.26.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.9.25-ldflags.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.9.25.plzcompile.patch.gz | patch -p1 -E --backup --verbose || exit 1

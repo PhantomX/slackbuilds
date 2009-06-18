2fc66f9d9e3eef486fdb5253af3f2654479b11e9
Phantom X <megaphantomx@bol.com.br>
2009-06-18 02:45:29 -0300

    adobereader, fakeroot, fwbuilder, inputproto, iptables, libfwbuilder, libquicktime, libusb1, vdrift, virtualbox, vlgothic-fonts, wireshark

--- sane/apply-patches.sh
+++ sane/apply-patches.sh
@@ -2,7 +2,11 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${BNAME}-1.0.20-rpath.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${BNAME}-1.0.19-glibc-2.7.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${BNAME}-1.0.20-pkgconfig.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${BNAME}-1.0.20-open-macro.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${BNAME}-1.0.20-man-utf8.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${BNAME}-1.0.20-epson-expression800.patch.gz | patch -p1 -E --backup --verbose || exit 1
+bzcat ${CWD}/${BNAME}-1.0.20-autoreconf.patch.bz2 | patch -p1 -E --backup --verbose || exit 1
 
 ( cd ${FSRCDIR} || exit 1
   zcat ${SB_PATCHDIR}/${FNAME}-1.0.14-badcode.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- wireshark/apply-patches.sh
+++ wireshark/apply-patches.sh
@@ -1,13 +1,7 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-1.0.2-pie.patch.gz | patch -p1 -E --backup --verbose || exit 1
+#zcat ${SB_PATCHDIR}/${NAME}-1.0.2-pie.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-nfsv4-opts.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.99.7-path.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-nfsv41.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-types.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-nfsv41-layout-updates.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-rpc-pdu-size.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.0.5-nfs41-backchnl-decode.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.0.6-netdump2.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}_pipe.patch.gz | patch -p1 -E --backup --verbose || exit 1

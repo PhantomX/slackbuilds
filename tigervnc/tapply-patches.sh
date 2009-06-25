188a3d2960d70336ee7af5b216f73712bddca59b
Phantom X <megaphantomx@bol.com.br>
2009-06-25 01:46:37 -0300

    highlight, htop, isomaster, libcanberra, mesa, mpd, nspr, pixman, qtcurve-{gtk2,kde3,kde4}, subversion, tigervnc, virtualbox, wine

--- tigervnc/tapply-patches.sh
+++ tigervnc/tapply-patches.sh
@@ -2,9 +2,6 @@
 SB_PATCHDIR=${CWD}/tpatches
 
 zcat ${SB_PATCHDIR}/${NAME}-102434.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-bounds.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-cookie.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-viewer-reparent.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}10-rh499401.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}10-rh497592.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}10-rh501832.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}10-rh495457.patch.gz | patch -p0 -E --backup --verbose || exit 1

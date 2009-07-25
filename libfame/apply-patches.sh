50c144abf77eb441f59a339a1352c69d5a5f3791
Phantom X <megaphantomx@bol.com.br>
2009-07-25 03:48:33 -0300

    x86_64 batch #6
    ffmpeg, mplayer, xine-lib

--- libfame/apply-patches.sh
+++ libfame/apply-patches.sh
@@ -6,3 +6,4 @@ zcat ${SB_PATCHDIR}/${NAME}-config-rpath.patch.gz | patch -p0 -E --backup --verb
 zcat ${SB_PATCHDIR}/${NAME}-gccver.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-nomarch.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.9.1-gcc34-1.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.9.1-gcc43.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- xine-lib/apply-patches.sh
+++ xine-lib/apply-patches.sh
@@ -7,3 +7,8 @@ touch -r m4/optimizations.m4.stamp m4/optimizations.m4
 touch -r m4/pthreads.m4 m4/pthreads.m4.stamp
 touch -r m4/pthreads.m4.stamp m4/pthreads.m4
 zcat ${SB_PATCHDIR}/${NAME}-1.1.1-deepbind-939.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# http://bugzilla.redhat.com/477226
+zcat ${SB_PATCHDIR}/${NAME}-1.1.16.2-multilib.patch.gz | patch -p1 -E --backup --verbose || exit 1
+## upstreamable patches
+zcat ${SB_PATCHDIR}/${NAME}-1.1.16.3-mod_mimetypes_typo.patch.gz | patch -p1 -E --backup --verbose || exit 1

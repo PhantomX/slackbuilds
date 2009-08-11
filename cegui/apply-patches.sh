ec9e8a38e55343dbb29c0af304ef653e344dbd47
Phantom X <megaphantomx@bol.com.br>
2009-08-11 03:12:42 -0300

    x86_64 batch #16

--- cegui/apply-patches.sh
+++ cegui/apply-patches.sh
@@ -3,3 +3,4 @@ SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${NAME}-0.6.0-userverso.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.6.0-release-as-so-ver.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-0.6.2-new-DevIL.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- fpc/apply-patches.sh
+++ fpc/apply-patches.sh
@@ -1,7 +1,5 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-2.2.2-samplecfg_32and64bit.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.2.2-fpcdocs.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.2.2-stackexecute.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.2.2-gtk2-r11651.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.2.4-samplecfg_32and64bit.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.2.4-stackexecute.patch.gz | patch -p0 -E --backup --verbose || exit 1

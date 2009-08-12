6b053880408f1b5ad14e5188ea2a52e774a3a3b6
Phantom X <megaphantomx@bol.com.br>
2009-08-12 04:21:32 -0300

    x86_64 batch #17, some games

--- allegro/apply-patches.sh
+++ allegro/apply-patches.sh
@@ -7,7 +7,7 @@ zcat ${SB_PATCHDIR}/${NAME}-4.2.0-nostrip.patch.gz | patch -p1 -E --backup --ver
 zcat ${SB_PATCHDIR}/${NAME}-4.2.0-noexecstack.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.2.0-multilib.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.2.1-noexecmod.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-4.2.2-libdir.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-4.0.3-libdir.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.2.2-pulseaudio.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.2.2-gcc43.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-4.2.2-gcc43-asm.patch.gz | patch -p1 -E --backup --verbose || exit 1

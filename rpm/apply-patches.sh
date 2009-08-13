1568088c59c7b4d4461893f834d5c7a87bfd0309
Phantom X <megaphantomx@bol.com.br>
2009-08-13 04:57:19 -0300

    rpm

--- rpm/apply-patches.sh
+++ rpm/apply-patches.sh
@@ -6,14 +6,7 @@ zcat ${SB_PATCHDIR}/rpm-4.5.90-pkgconfig-path.patch.gz | patch -p1 -E --backup -
 zcat ${SB_PATCHDIR}/rpm-4.5.90-gstreamer-provides.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # Patches already in upstream
-zcat ${SB_PATCHDIR}/rpm-4.7.0-findlang-kde3.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-prtsig.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-python-altnevr.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-hardlink-sizes.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-dwarf3.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-osgideps.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-fp-symlink.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/rpm-4.7.0-fp-findbyfile.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
+# These are not yet upstream
 zcat ${SB_PATCHDIR}/rpm-4.7.0-extra-provides.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/rpm-4.6.0-niagara.patch.gz | patch -p1 -E --backup --verbose || exit 1

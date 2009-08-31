aee015676e31e529dfb707cb0d57d46e95184081
Phantom X <megaphantomx@bol.com.br>
2009-08-31 02:31:13 -0300

    arora, binutils, dejavu-fonts-ttf, gst-plugins-{bad,good}, libcap, libusb1, preload

--- binutils/apply-patches.sh
+++ binutils/apply-patches.sh
@@ -1,5 +1,8 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.1-symbolic-envvar-revert.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.10-libtool-lib64.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.10-envvar-revert.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.1-set-long-long.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.51.0.11-moxie.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.51.0.14-unique-is-global.patch.gz | patch -p0 -E --backup --verbose || exit 1

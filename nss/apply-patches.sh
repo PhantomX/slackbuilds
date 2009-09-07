98c4e0b886d5eb6e19ca4789cfbc87eebd6ff771
Phantom X <megaphantomx@bol.com.br>
2009-09-07 03:47:44 -0300

    brofficeorg, dpkg, libsilc, nss, pidgin, qbittorrent, udev

--- nss/apply-patches.sh
+++ nss/apply-patches.sh
@@ -5,7 +5,4 @@ SB_PATCHDIR=${CWD}/patches
 zcat ${SB_PATCHDIR}/${NAME}-no-rpath.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-nolocalsql.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-enable-pem.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-stubs-bug502133.patch.gz | patch -p0 -E --backup --verbose || exit 1
-if [ "${ARCH}" = "x86_64" ] ;then
-  zcat ${SB_PATCHDIR}/${NAME}-fix-zlib.patch.gz | patch -p0 -E --backup --verbose || exit 1
-fi
+zcat ${SB_PATCHDIR}/${NAME}-pem509705.patch.gz | patch -p0 -E --backup --verbose || exit 1

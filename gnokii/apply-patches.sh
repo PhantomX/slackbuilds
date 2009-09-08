6e7942c4e09610db08db84154795662a706eb3b6
Phantom X <megaphantomx@bol.com.br>
2009-09-08 03:25:23 -0300

    farsight2, gnokii, gsmartcontrol, minitube, util-linux-ng

--- gnokii/apply-patches.sh
+++ gnokii/apply-patches.sh
@@ -1,8 +1,6 @@
 
 SB_PATCHDIR=${CWD}/patches
 
-# Patch to make gnokii use "htmlview" instead of "mozilla" as default browser
-zcat ${SB_PATCHDIR}/${NAME}-htmlview.patch.gz | patch -p0 -E --backup --verbose || exit 1
 # Patch to remove port locking and apply the system-wide /usr/sbin directory
 # to the path instead of the default /usr/local
 zcat ${SB_PATCHDIR}/${NAME}-config.patch.gz | patch -p0 -E --backup --verbose || exit 1

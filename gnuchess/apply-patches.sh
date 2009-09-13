849066c687ce52519cadc5449f29c9bb027dadeb
Phantom X <megaphantomx@bol.com.br>
2009-09-13 01:08:32 -0300

    bzr, bzrtools, gnuchess, hdparm, libxml2, squid, taglib-extras

--- gnuchess/apply-patches.sh
+++ gnuchess/apply-patches.sh
@@ -11,9 +11,5 @@ zcat ${SB_PATCHDIR}/${NAME}-5.07-glibc.patch.gz | patch -p0 --verbose --backup -
 ) || exit 1
 
 ( cd ${XBSRCDIR} || exit 1
-  # --infodir is fubar.
-  zcat ${SB_PATCHDIR}/${XBNAME}.infodir.diff.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
-  # Taken from Debian to fix CVE-2004-2552
-  # FOR WHICH THERE IS NO ATTACK VECTOR!!!
-  zcat ${SB_PATCHDIR}/${XBNAME}.buffer_overflow_fix.diff.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
+  zcat ${SB_PATCHDIR}/12_fix_chess_engine.patch.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
 ) || exit 1

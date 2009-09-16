dcff619fe4499023e1d2f7da1c1d64b87805a521
Phantom X <megaphantomx@bol.com.br>
2009-09-16 03:13:06 -0300

    chkrootkit, doxygen, gmime, iptables, kernel-custom, klavaro, libsigc++, minitube, wireshark

--- chkrootkit/apply-patches.sh
+++ chkrootkit/apply-patches.sh
@@ -6,6 +6,4 @@ zcat ${SB_PATCHDIR}/${NAME}-0.44-inetd.patch.gz | patch -p1 -E --backup --verbos
 zcat ${SB_PATCHDIR}/${NAME}-0.45-includes.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.47-warnings.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.47-chklastlog.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.48-anomalies.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.48-nophpcheck.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-0.48-chkproc-psver.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- kernel-custom/apply-patches.sh
+++ kernel-custom/apply-patches.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 SB_PATCHDIR=${CWD}/patches
 
 # Set to test (some patches require others, so, is not 100%)
@@ -19,71 +21,87 @@ fi
 
 PATCHCOM="patch ${DRYRUN_OPT} -p1 -F1 -s ${VERBOSE_OPT}"
 
-# Adds layer 7 iptables support
-#zcat ${SB_PATCHDIR}/kernel-2.6.25-layer7-2.20.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch() {
+  local patch=$1
+  shift
+  if [ ! -f ${SB_PATCHDIR}/${patch} ]; then
+    exit 1
+  fi
+  case "${patch}" in
+  *.bz2) bzcat "${SB_PATCHDIR}/${patch}" | ${PATCHCOM} ${1+"$@"} ;;
+  *.gz) zcat "${SB_PATCHDIR}/${patch}" | ${PATCHCOM} ${1+"$@"} ;;
+  *) ${PATCHCOM} ${1+"$@"} -i "${SB_PATCHDIR}/${patch}" ;;
+  esac
+}
 
-zcat ${SB_PATCHDIR}/acpi-dsdt-initrd-v0.9d-2.6.30-20090730+log.patch.gz | ${PATCHCOM} || exit 1
+ApplyOptionalPatch() {
+  local patch=$1
+  shift
+  if [ ! -f ${SB_PATCHDIR}/${patch} ]; then
+    exit 1
+  fi
+  local C=$(ApplyPatch ${patch} | wc -l | awk '{print $1}')
+  if [ "${C}" -gt 9 ]; then
+    ApplyPatch ${patch} ${1+"$@"}
+  fi
+}
+
+# Adds layer 7 iptables support
+#ApplyPatch kernel-2.6.25-layer7-2.20.patch.gz
+
+ApplyPatch acpi-dsdt-initrd-v0.9d-2.6.30-20090730+log.patch.gz
 
 # This patch adds a "make nonint_oldconfig" which is non-interactive and
 # also gives a list of missing options at the end. Useful for automated
 # builds (as used in the buildsystem).
-zcat ${SB_PATCHDIR}/linux-2.6-build-nonintconfig.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-build-nonintconfig.patch.gz
 
-zcat ${SB_PATCHDIR}/linux-2.6-makefile-after_link.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-makefile-after_link.patch.gz
 
 #
 # misc small stuff to make things compile
 #
-C=$(zcat ${SB_PATCHDIR}/linux-2.6-compile-fixes.patch.gz | wc -l | awk '{print $1}')
-if [ "$C" -gt 10 ]; then
-  zcat ${SB_PATCHDIR}/linux-2.6-compile-fixes.patch.gz | ${PATCHCOM} || exit 1
-fi
+ApplyOptionalPatch linux-2.6-compile-fixes.patch.gz
 
 # revert patches from upstream that conflict or that we get via other means
-C=$(zcat ${SB_PATCHDIR}/linux-2.6-upstream-reverts.patch.gz | wc -l | awk '{print $1}')
-if [ "$C" -gt 10 ]; then
-  zcat ${SB_PATCHDIR}/linux-2.6-upstream-reverts.patch.gz | ${PATCHCOM} -R || exit 1
-fi
+ApplyOptionalPatch linux-2.6-upstream-reverts.patch.gz | ${PATCHCOM} -R || exit 1
 
-C=$(zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | wc -l | awk '{print $1}')
-if [ "$C" -gt 10 ]; then
-  zcat ${SB_PATCHDIR}/linux-2.6-hotfixes.patch.gz | ${PATCHCOM} || exit 1
-fi
+ApplyOptionalPatch linux-2.6-hotfixes.patch.gz
 
 # Roland's utrace ptrace replacement.
-zcat ${SB_PATCHDIR}/linux-2.6-tracehook.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-utrace.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-utrace-ftrace.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-tracehook.patch.gz
+ApplyPatch linux-2.6-utrace.patch.gz
+ApplyPatch linux-2.6-utrace-ftrace.patch.gz
 
 # vm patches
-zcat ${SB_PATCHDIR}/linux-2.6-defaults-saner-vm-settings.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-mm-lru-evict-streaming-io-pages-first.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-mm-lru-report-vm-flags-in-page-referenced.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-mm-lru-dont-evict-mapped-executable-pages.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-defaults-saner-vm-settings.patch.gz
+ApplyPatch linux-2.6-mm-lru-evict-streaming-io-pages-first.patch.gz
+ApplyPatch linux-2.6-mm-lru-report-vm-flags-in-page-referenced.patch.gz
+ApplyPatch linux-2.6-mm-lru-dont-evict-mapped-executable-pages.patch.gz
 
 # enable sysrq-c on all kernels, not only kexec
-zcat ${SB_PATCHDIR}/linux-2.6-sysrq-c.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-sysrq-c.patch.gz
 
 # Architecture patches
 # x86(-64)
-zcat ${SB_PATCHDIR}/linux-2.6-cpufreq-enable-acpi-pstates-on-via.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-hwmon-temp-sensor.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-padlock-10-enable-64bit.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-padlock-20-add-x86-dependency.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-padlock-30-fix-might-sleep.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-padlock-40-nano-ecb.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-padlock-50-nano-cbc.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-rng-enable-64bit.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/via-sdmmc.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-x86-delay-tsc-barrier.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-cpufreq-enable-acpi-pstates-on-via.patch.gz
+ApplyPatch via-hwmon-temp-sensor.patch.gz
+ApplyPatch via-padlock-10-enable-64bit.patch.gz
+ApplyPatch via-padlock-20-add-x86-dependency.patch.gz
+ApplyPatch via-padlock-30-fix-might-sleep.patch.gz
+ApplyPatch via-padlock-40-nano-ecb.patch.gz
+ApplyPatch via-padlock-50-nano-cbc.patch.gz
+ApplyPatch via-rng-enable-64bit.patch.gz
+ApplyPatch via-sdmmc.patch.gz
+ApplyPatch linux-2.6-x86-delay-tsc-barrier.patch.gz
 
 # Export xfrm[4|6] gc_thresh values to sysctl
-zcat ${SB_PATCHDIR}/linux-2.6-xfrm-export-gc_thresh.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-xfrm-export-gc_thresh.patch.gz
 
 #
 # Exec shield
 #
-zcat ${SB_PATCHDIR}/linux-2.6-execshield.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-execshield.patch.gz
 
 #
 # bugfixes to drivers and filesystems
@@ -97,28 +115,28 @@ zcat ${SB_PATCHDIR}/linux-2.6-execshield.patch.gz | ${PATCHCOM} || exit 1
 
 # cifs
 # fix cifs mount option "port=" (#506574)
-zcat ${SB_PATCHDIR}/linux-2.6-fs-cifs-fix-port-numbers.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-fs-cifs-fix-port-numbers.patch.gz
 
 # USB
 
 # ACPI
-zcat ${SB_PATCHDIR}/linux-2.6-defaults-acpi-video.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-acpi-video-dos.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-defaults-acpi-video.patch.gz
+ApplyPatch linux-2.6-acpi-video-dos.patch.gz
 
 # Various low-impact patches to aid debugging.
-zcat ${SB_PATCHDIR}/linux-2.6-debug-sizeof-structs.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-debug-nmi-timeout.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-debug-taint-vm.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-debug-spinlock-taint.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-debug-sizeof-structs.patch.gz
+ApplyPatch linux-2.6-debug-nmi-timeout.patch.gz
+ApplyPatch linux-2.6-debug-taint-vm.patch.gz
+ApplyPatch linux-2.6-debug-spinlock-taint.patch.gz
 ## try to find out what is breaking acpi-cpufreq
-zcat ${SB_PATCHDIR}/linux-2.6-debug-vm-would-have-oomkilled.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/linux-2.6-debug-always-inline-kzalloc.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-debug-vm-would-have-oomkilled.patch.gz
+ApplyPatch linux-2.6-debug-always-inline-kzalloc.patch.gz
 
 #
 # PCI
 #
 # disable message signaled interrupts
-zcat ${SB_PATCHDIR}/linux-2.6-defaults-pci_no_msi.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-defaults-pci_no_msi.patch.gz
 
 #
 # SCSI Bits.
@@ -126,8 +144,8 @@ zcat ${SB_PATCHDIR}/linux-2.6-defaults-pci_no_msi.patch.gz | ${PATCHCOM} || exit
 
 # ALSA
 # squelch hda_beep by default
-zcat ${SB_PATCHDIR}/linux-2.6-defaults-alsa-hda-beep-off.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/hda_intel-prealloc-4mb-dmabuffer.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-defaults-alsa-hda-beep-off.patch.gz
+ApplyPatch hda_intel-prealloc-4mb-dmabuffer.patch.gz
 
 # block/bio
 #
@@ -135,45 +153,45 @@ zcat ${SB_PATCHDIR}/hda_intel-prealloc-4mb-dmabuffer.patch.gz | ${PATCHCOM} || e
 # Filesystem patches.
 
 # Networking
-zcat ${SB_PATCHDIR}/linux-2.6-missing-rfc2465-stats.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-missing-rfc2465-stats.patch.gz
 
 # neigh: fix state transition INCOMPLETE->FAILED via Netlink request
-zcat ${SB_PATCHDIR}/linux-2.6-neigh_-fix-state-transition-INCOMPLETE-_FAILED-via-Netlink-request.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-neigh_-fix-state-transition-INCOMPLETE-_FAILED-via-Netlink-request.patch.gz
 
 # add ich9 lan
-zcat ${SB_PATCHDIR}/linux-2.6-e1000-ich9.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-e1000-ich9.patch.gz
 
 # Misc fixes
 # The input layer spews crap no-one cares about.
-zcat ${SB_PATCHDIR}/linux-2.6-input-kill-stupid-messages.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-input-kill-stupid-messages.patch.gz
 
 # Get away from having to poll Toshibas
-zcat ${SB_PATCHDIR}/linux-2.6-input-fix-toshiba-hotkeys.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-input-fix-toshiba-hotkeys.patch.gz
 
 # HID: add support for Bluetooth Wacom pads
-zcat ${SB_PATCHDIR}/linux-2.6-input-wacom-bluetooth.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-input-wacom-bluetooth.patch.gz
 
 # Allow to use 480600 baud on 16C950 UARTs
-zcat ${SB_PATCHDIR}/linux-2.6-serial-460800.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-serial-460800.patch.gz
 
 # fix oops in nozomi drver (#507005) plus two others
-zcat ${SB_PATCHDIR}/linux-2.6-drivers-char-low-latency-removal.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-drivers-char-low-latency-removal.patch.gz
 # let users skip the TXEN bug test
-zcat ${SB_PATCHDIR}/linux-2.6-serial-add-txen-test-param.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-serial-add-txen-test-param.patch.gz
 
 # Silence some useless messages that still get printed with 'quiet'
-zcat ${SB_PATCHDIR}/linux-2.6-silence-noise.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-silence-noise.patch.gz
 
 # Make fbcon not show the penguins with 'quiet'
-zcat ${SB_PATCHDIR}/linux-2.6-silence-fbcon-logo.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-silence-fbcon-logo.patch.gz
 
 # Changes to upstream defaults.
 
 # back-port scan result aging patches
-#zcat ${SB_PATCHDIR}/linux-2.6-mac80211-age-scan-results-on-resume.patch.gz | ${PATCHCOM} || exit 1
+#ApplyPatch linux-2.6-mac80211-age-scan-results-on-resume.patch.gz
 
 # iwlwifi: fix TX queue race
-zcat ${SB_PATCHDIR}/linux-2.6-iwlwifi_-fix-TX-queue-race.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-iwlwifi_-fix-TX-queue-race.patch.gz
 
 # libata
 
@@ -182,52 +200,72 @@ zcat ${SB_PATCHDIR}/linux-2.6-iwlwifi_-fix-TX-queue-race.patch.gz | ${PATCHCOM} 
 #
 
 # /dev/crash driver.
-zcat ${SB_PATCHDIR}/linux-2.6-crash-driver.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-crash-driver.patch.gz
 
 # http://www.lirc.org/
-zcat ${SB_PATCHDIR}/lirc-2.6.31.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/hid-ignore-all-recent-imon-devices.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/hdpvr-ir-enable.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/lirc-revert-2.6.31-i2c-changes.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch lirc-2.6.31.patch.gz
+ApplyPatch hid-ignore-all-recent-imon-devices.patch.gz
+ApplyPatch hdpvr-ir-enable.patch.gz
+ApplyPatch lirc-revert-2.6.31-i2c-changes.patch.gz
 
-zcat ${SB_PATCHDIR}/agp-set_memory_ucwb.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch agp-set_memory_ucwb.patch.gz
 # Nouveau DRM + drm fixes
-#zcat ${SB_PATCHDIR}/drm-next.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-modesetting-radeon.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-nouveau.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-no-gem-on-i8xx.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-i915-resume-force-mode.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-intel-big-hammer.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-intel-gen3-fb-hack.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-intel-hdmi-edid-fix.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-modesetting-radeon-fixes.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-radeon-new-pciids.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-dont-frob-i2c.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-pnp-add-resource-range-checker.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/drm-i915-enable-mchbar.patch.gz | ${PATCHCOM} || exit 1
+#ApplyPatch drm-next.patch.gz
+ApplyPatch drm-modesetting-radeon.patch.gz
+ApplyPatch drm-nouveau.patch.gz
+ApplyPatch drm-no-gem-on-i8xx.patch.gz
+ApplyPatch drm-i915-resume-force-mode.patch.gz
+ApplyPatch drm-intel-big-hammer.patch.gz
+ApplyPatch drm-intel-gen3-fb-hack.patch.gz
+ApplyPatch drm-intel-hdmi-edid-fix.patch.gz
+ApplyPatch drm-modesetting-radeon-fixes.patch.gz
+ApplyPatch drm-radeon-new-pciids.patch.gz
+ApplyPatch drm-dont-frob-i2c.patch.gz
+ApplyPatch drm-pnp-add-resource-range-checker.patch.gz
+ApplyPatch drm-i915-enable-mchbar.patch.gz
 
 # linux1394 git patches
-#zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-update.patch.gz | ${PATCHCOM} || exit 1
-#C=$(zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | wc -l | awk '{print $1}')
-#if [ "$C" -gt 10 ]; then
-  #zcat ${SB_PATCHDIR}/linux-2.6-firewire-git-pending.patch.gz | ${PATCHCOM} || exit 1
-#fi
+#ApplyPatch linux-2.6-firewire-git-update.patch.gz
+#ApplyOptionalPatch linux-2.6-firewire-git-pending.patch.gz
 
 # silence the ACPI blacklist code
-zcat ${SB_PATCHDIR}/linux-2.6-silence-acpi-blacklist.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-silence-acpi-blacklist.patch.gz
 
 # kvm
-zcat ${SB_PATCHDIR}/linux-2.6-kvm-skip-pit-check.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-kvm-skip-pit-check.patch.gz
 
 # xen
-zcat ${SB_PATCHDIR}/linux-2.6.29-xen-disable-gbpages.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6.29-xen-disable-gbpages.patch.gz
 
 # v12n
-zcat ${SB_PATCHDIR}/linux-2.6-virtio_blk-dont-bounce-highmem-requests.patch.gz | ${PATCHCOM} || exit 1
+ApplyPatch linux-2.6-virtio_blk-dont-bounce-highmem-requests.patch.gz
 
 # V4L/DVB updates/fixes/experimental drivers
-#zcat ${SB_PATCHDIR}/linux-2.6-v4l-dvb-fixes.patch.gz | ${PATCHCOM} || exit 1
-#zcat ${SB_PATCHDIR}/linux-2.6-v4l-dvb-update.patch.gz | ${PATCHCOM} || exit 1
-#zcat ${SB_PATCHDIR}/linux-2.6-v4l-dvb-experimental.patch.gz | ${PATCHCOM} || exit 1
-#zcat ${SB_PATCHDIR}/linux-2.6-revert-dvb-net-kabi-change.patch.gz | ${PATCHCOM} || exit 1
-zcat ${SB_PATCHDIR}/v4l-dvb-fix-cx25840-firmware-load.patch.gz | ${PATCHCOM} || exit 1
+#ApplyPatch linux-2.6-v4l-dvb-fixes.patch.gz
+#ApplyPatch linux-2.6-v4l-dvb-update.patch.gz
+#ApplyPatch linux-2.6-v4l-dvb-experimental.patch.gz
+#ApplyPatch linux-2.6-revert-dvb-net-kabi-change.patch.gz
+ApplyPatch v4l-dvb-fix-cx25840-firmware-load.patch.gz
+
+ApplyPatch make-mmap_min_addr-suck-less.patch.gz
+
+# ----- send for upstream inclusion -----
+ApplyPatch linux-2.6-cifs-reenable-lanman-security.patch.gz
+
+# ----- patches headed for -stable -----
+
+# CVE-2009-2847
+
+# Fix string overflows found by stackprotector:
+ApplyPatch hda-check-strcpy-length.patch.gz
+ApplyPatch linux-2.6-v4l-dvb-af9015-fix-stack-corruption.patch.gz
+
+# fix stack protector problems with xen on x86_64
+ApplyPatch linux-2.6-x86-load-percpu-segment-no-stackprotector.patch.gz
+ApplyPatch linux-2.6-xen-rearrange-to-fix-stackprotector.patch.gz
+
+# fix lockdep warnings in cpufreq (#522685)
+ApplyPatch linux-2.6-cpufreq-eliminate-lockdep-warnings.patch.gz
+ApplyPatch linux-2.6-cpufreq-cleanup-locking-in-ondemand.patch.gz
+
+set +e

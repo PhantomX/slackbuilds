71fb7383ffe06b63e43699393b23923dcbb4536e
Phantom X <megaphantomx@bol.com.br>
2009-09-24 01:53:29 -0300

    atk, glib2, gobject-introspection, gtk+2, gvfs, libsoup, pango, webkit and snome gnome 2.28.0.

--- gtk+2/apply-patches.sh
+++ gtk+2/apply-patches.sh
@@ -10,8 +10,8 @@ zcat ${SB_PATCHDIR}/gtk.pnglz.diff.gz | patch -p1 -E --backup --verbose || exit 
 # using /etc/gtk-2.0/$host instead of /etc/gtk-2.0/ now):
 zcat ${SB_PATCHDIR}/gtk.etc.host.location.diff.gz | patch -p1 --verbose || exit 1
 
-# http://bugzilla.redhat.com/show_bug.cgi?id=478400
-zcat ${SB_PATCHDIR}/default_printer.patch.gz | patch -p0 -E --backup --verbose || exit 1
+# https://bugzilla.gnome.org/show_bug.cgi?id=566522
+zcat ${SB_PATCHDIR}/gtk2-printing-smb-auth.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
-# http://bugzilla.gnome.org/show_bug.cgi?id=384940
-zcat ${SB_PATCHDIR}/print_authentication.patch.gz | patch -p0 -E --backup --verbose || exit 1
+# https://bugzilla.gnome.org/show_bug.cgi?id=586207
+zcat ${SB_PATCHDIR}/gtk2-printing-nonblocking-printer-list.patch.gz | patch -p1 -E --backup --verbose || exit 1

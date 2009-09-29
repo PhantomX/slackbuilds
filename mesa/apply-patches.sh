6d1d21eddde6433f84028cdcd00177c9475558bd
Phantom X <megaphantomx@bol.com.br>
2009-09-29 02:20:44 -0300

    More gnome 2.28.0

--- mesa/apply-patches.sh
+++ mesa/apply-patches.sh
@@ -2,4 +2,5 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/mesa-7.1-link-shared.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/mesa-no-mach64.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/mesa-no-mach64.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/mesa-7.6-hush-vblank-warning.patch.gz | patch -p1 -E --backup --verbose || exit 1

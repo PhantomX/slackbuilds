ecf3d9b67ffcde378776e8fc278c66abfd63fff3
Phantom X <megaphantomx@bol.com.br>
2009-10-11 02:55:10 -0300

    binutils, freetype, gparted, kbd, libexif, and more xorg 7.5 updates, including all font packages.

--- binutils/apply-patches.sh
+++ binutils/apply-patches.sh
@@ -1,7 +1,6 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.10-libtool-lib64.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.10-envvar-revert.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.1-set-long-long.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.19.51.0.11-moxie.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.51.0.10-libtool-lib64.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.51.0.10-envvar-revert.patch.gz | patch -p0 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-2.19.50.0.10-set-long-long.patch.gz | patch -p0 -E --backup --verbose || exit 1
--- kbd/apply-patches.sh
+++ kbd/apply-patches.sh
@@ -3,7 +3,6 @@ SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/nl.euro.diff.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.15-po.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-1.15-sparc.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.15-keycodes-man.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.15-unicode_start.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.15-resizecon-x86_64.patch.gz | patch -p1 -E --backup --verbose || exit 1

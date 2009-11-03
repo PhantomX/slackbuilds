990d840f4dbc8697263bdd00c373663d71c76992
Phantom X <megaphantomx@bol.com.br>
2009-11-03 03:49:25 -0200

    gnutls, goffice, iso-codes, lazarus, libao, libisoburn, nspr, picard, taglib, transcode, xf86-input-joystick, zoneinfo

--- gnuchess/apply-patches.sh
+++ gnuchess/apply-patches.sh
@@ -5,11 +5,4 @@ SB_PATCHDIR=${CWD}/patches
 zcat ${SB_PATCHDIR}/${NAME}.gcc4_fix.diff.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
 # Fix for glibc >= 2.9.90
 zcat ${SB_PATCHDIR}/${NAME}-5.07-glibc.patch.gz | patch -p0 --verbose --backup --suffix=.orig || exit 1
-
-( cd ${EBSRCDIR} || exit 1
-  zcat ${SB_PATCHDIR}/${EBNAME}-1.1.1-gcc44.patch.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
-) || exit 1
-
-( cd ${XBSRCDIR} || exit 1
-  zcat ${SB_PATCHDIR}/12_fix_chess_engine.patch.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
-) || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-5.06-bookpath.patch.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1

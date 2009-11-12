9071deb5aaccb53394b950e29ef33e6620ef443f
Phantom X <megaphantomx@bol.com.br>
2009-11-12 03:37:53 -0200

    allegro, constantine-backgrounds, fakeroot, gedit, gstreamermm, leonidas-backgrounds, lxappearance, pulseaudio, udev, xgc, xkbprint, xterm

--- udev/apply-patches.sh
+++ udev/apply-patches.sh
@@ -3,5 +3,4 @@ SB_PATCHDIR=${CWD}/patches
 
 zcat ${CWD}/rule_generator.patch.gz | patch -p1 --verbose || exit 1
 zcat ${SB_PATCHDIR}/udev-118-sysconf.patch.gz | patch -p1 --verbose || exit 1
-# Some ancient apps expects js in /dev, so revert to old behavior (symlink in /dev)
-zcat ${SB_PATCHDIR}/udev-136-old-js-behavior.patch.gz | patch -p0 --verbose || exit 1
+
--- xterm/apply-patches.sh
+++ xterm/apply-patches.sh
@@ -2,5 +2,4 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/${NAME}-242-resources.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-222-can-2003-0063.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-226-man-page_paths.patch.gz | patch -p1 -E --backup --verbose || exit 1

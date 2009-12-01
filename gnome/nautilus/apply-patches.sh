b29c859cfe8a867286123d0cc80640e3c8d89fad
Phantom X <megaphantomx@bol.com.br>
2009-12-01 05:17:45 -0200

    gajim, goffice, gtk+2, gvfs, libusb1, nautilus, pidgin, rkhunter, virtualbox

--- gnome/nautilus/apply-patches.sh
+++ gnome/nautilus/apply-patches.sh
@@ -10,7 +10,6 @@ zcat ${SB_PATCHDIR}/${NAME}-config.patch.gz | patch -p1 -E --backup --verbose ||
 zcat ${SB_PATCHDIR}/${NAME}-2.5.7-rhconfig.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
 # from upstream
-zcat ${SB_PATCHDIR}/${NAME}-2.28.1-tracker-0.7-failed-connection.patch.gz | patch -p1 -E --backup --verbose || exit 1
 # https://bugzilla.gnome.org/show_bug.cgi?id=589345
 zcat ${SB_PATCHDIR}/${NAME}-2.28.1-dynamic-search.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/rtl-fix.patch.gz | patch -p1 -E --backup --verbose || exit 1
@@ -22,3 +21,6 @@ zcat ${SB_PATCHDIR}/${NAME}-filetype-symlink-fix.patch.gz | patch -p0 -E --backu
 
 # Need to file upstream and investigate a real fix
 zcat ${SB_PATCHDIR}/${NAME}-2.28.0-revert-bg-fade-break.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+# https://bugzilla.gnome.org/show_bug.cgi?id=147808
+zcat ${SB_PATCHDIR}/${NAME}-monitor-change.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- pidgin/apply-patches.sh
+++ pidgin/apply-patches.sh
@@ -1,6 +1,2 @@
   
 SB_PATCHDIR=${CWD}/patches
-
-zcat ${SB_PATCHDIR}/${NAME}-2.6.2-yahoo-buddy-idle-time.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.6.2-yahoo-status-change-away.patch.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-2.6.2-crash-validate-jid.patch.gz | patch -p0 -E --backup --verbose || exit 1

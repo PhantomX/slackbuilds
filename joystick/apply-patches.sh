e56d6fe18ead632442bd639b84d6c90ace16d1a8
Phantom X <megaphantomx@bol.com.br>
2009-12-14 05:08:12 -0200

    bsnes, bsnesfilter, bsnesreader, jstest-gtk, libgnomekbd, joy2key, mkvtoolnix, prelink,  xboard

--- joystick/apply-patches.sh
+++ joystick/apply-patches.sh
@@ -2,8 +2,17 @@
 SB_PATCHDIR=${CWD}/patches
 
 if [ "${SNAPBUILD}" = "YES" ] ;then
-  zcat ${SB_PATCHDIR}/${NAME}-MCS-defines.patch.gz | patch -p0 -E --backup --verbose || exit 1
-  zcat ${SB_PATCHDIR}/${NAME}-jstest-segv.patch.gz | patch -p1 -E --backup --verbose || exit 1
+  zcat ${CWD}/${PSRCARCHIVE} | patch -p1 --verbose || exit 1
+  for patch in \
+    jitter-tolerance \
+    button-axis-remapping \
+    usb-joystick-segfault-fix \
+    remap-calibration \
+    button-map-segfault-fix \
+    ioctl-error-handling \
+  ; do
+    cat debian/patches/${patch}.patch | patch -p2 -E --backup --verbose || exit 1
+  done
 else
   zcat ${SB_PATCHDIR}/${NAME}-1.2.15-redhat.patch.gz | patch -p1 -E --backup --verbose || exit 1
   zcat ${SB_PATCHDIR}/${NAME}-1.2.15-newkernel.patch.gz | patch -p1 -E --backup --verbose || exit 1

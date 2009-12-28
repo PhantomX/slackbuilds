923532918d34c2b6d13ed6e9049c44d10b00dd46
Phantom X <megaphantomx@bol.com.br>
2009-12-28 03:20:04 -0200

    blktrace, bluez, clamtk, gstreamermm, libaio, libass, libburn, poppler, youtube-dl, zoneinfo

--- xvidcap/apply-patches.sh
+++ xvidcap/apply-patches.sh
@@ -5,3 +5,4 @@ zcat ${SB_PATCHDIR}/${NAME}-1.1.7-ffmpeg-decode-audio2.patch.gz | patch -p0 -E -
 zcat ${SB_PATCHDIR}/${NAME}-1.1.7-ffmpeg-headers.patch.gz | patch -p0 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.1.7-ffmpeg-trellis.patch.gz | patch -p1 -E --backup --verbose || exit 1
 zcat ${SB_PATCHDIR}/${NAME}-1.1.7-avutil.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-1.1.7-shmproto.patch.gz | patch -p0 -E --backup --verbose || exit 1

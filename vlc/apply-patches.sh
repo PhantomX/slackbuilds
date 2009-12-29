90aaf554da1b2db6d63c2bc7c8c9ec1dd441867a
Phantom X <megaphantomx@bol.com.br>
2009-12-29 04:03:05 -0200

    libdvbpsi, libkate, libmtp, libnetfilter_conntrack, libtiger, lives, mpd, opencv, pyopengl, unicap, vlc

---
+++ vlc/apply-patches.sh
@@ -0,0 +1,9 @@
+  
+SB_PATCHDIR=${CWD}/patches
+
+zcat ${SB_PATCHDIR}/vlc-trunk-default_font.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/0001-Default-libv4l2-to-true.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/300_all_pic.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/310_all_mmx_pic.patch.gz | patch -p1 -E --backup --verbose || exit 1
+sed -i.dmo_pic -e 's/fno-PIC/fPIC/' libs/loader/Makefile.in
+zcat ${SB_PATCHDIR}/320_all_disable_media_list_player_tests.patch.gz | patch -p1 -E --backup --verbose || exit 1

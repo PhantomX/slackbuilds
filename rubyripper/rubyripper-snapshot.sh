1dc76bd4e240c94b47406fe27116910448c32fdd
Phantom X <megaphantomx@bol.com.br>
2010-01-03 04:51:35 -0200

    2mandvd, cd-discid, fotoxx, gxine, libmpdclient, mercurial, mysql, ncmpcpp, neon, pakchois, parcellite, psmisc, ruby, ruby-{cairo,gettext,gtk2,locale}, rubyripper, wavegain, wavpack

---
+++ rubyripper/rubyripper-snapshot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+module=$(basename $0 -snapshot.sh)
+snaproot="git://github.com/rubyripperdev/${module}.git"
+
+tmp=$(mktemp -d)
+
+trap cleanup EXIT
+cleanup() {
+  set +e
+  [ -z "${tmp}" -o ! -d "${tmp}" ] || rm -rf "${tmp}"
+}
+
+unset CDPATH
+pwd=$(pwd)
+snap=${snap:-$(date +%Y%m%d)}
+
+[ "${snap}" = "$(date +%Y%m%d)" ] && SNAP_COOPTS="--depth 1"
+
+pushd "${tmp}"
+  git clone ${SNAP_COOPTS} ${snaproot} ${module}-${snap}
+  pushd ${module}-${snap}
+    if [ "${snap}" != "$(date +%Y%m%d)" ] ; then
+      gitdate="$(echo -n ${snap} | head -c -4)-$(echo -n ${snap} | tail -c -4|head -c -2)-$(echo -n ${snap} | tail -c -2)"
+      git checkout $(git rev-list -n 1 --before="${gitdate}" master)
+    fi
+    find . -type d -name .git -print0 | xargs -0r rm -rf
+    rm -f .gitignore config.git-hash
+  popd
+  tar -Jcf "${pwd}"/${module}-${snap}.tar.xz ${module}-${snap}
+popd >/dev/null

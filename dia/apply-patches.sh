8af88cd33d9a3868144b0d57bb3b209e844f2180
Phantom X <megaphantomx@bol.com.br>
2010-01-25 03:40:09 -0200

    bluez, bzr, dia, gphoto2, grub, libgphoto2, qemu, qmpdclient, xterm

--- dia/apply-patches.sh
+++ dia/apply-patches.sh
@@ -7,8 +7,3 @@ zcat ${SB_PATCHDIR}/${NAME}-0.97.0-gnome-doc.patch.gz | patch -p1 -E --backup --
 # Fix compilation python
 zcat ${SB_PATCHDIR}/${NAME}-0.97-acinclude-python-fixes.patch.gz | patch -p1 -E --backup --verbose || exit 1
 
-# Fix automagic libEMF
-zcat ${SB_PATCHDIR}/${NAME}-0.97-automagic-libemf.patch.gz | patch -p1 -E --backup --verbose || exit 1
-
-# Fix bashism
-zcat ${SB_PATCHDIR}/${NAME}-0.97-fix-bashism.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- qemu/apply-patches.sh
+++ qemu/apply-patches.sh
@@ -1,7 +1,2 @@
   
 SB_PATCHDIR=${CWD}/patches
-
-( cd ${KSRCDIR}
-  zcat ${SB_PATCHDIR}/kvm-upstream-ppc.patch.gz | patch -p1 -E --backup --verbose || exit 1
-  zcat ${SB_PATCHDIR}/kvm-bios.diff.gz | patch -p1 -E --backup --verbose || exit 1
-) || exit 1

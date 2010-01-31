18ffa6e5acd806b1b190cd2e51709cbbc977fdbc
Phantom X <megaphantomx@bol.com.br>
2010-01-31 04:10:24 -0200

    gmime, gnome-power-manager, jmf, metacity, libthai, qbittorrent, vlc

--- gnome/metacity/apply-patches.sh
+++ gnome/metacity/apply-patches.sh
@@ -9,9 +9,3 @@ zcat ${SB_PATCHDIR}/stop-spamming-xsession-errors.patch.gz | patch -p1 -E --back
 zcat ${SB_PATCHDIR}/dnd-keynav.patch
 # http://bugzilla.gnome.org/show_bug.cgi?id=588119
 zcat ${SB_PATCHDIR}/Should-set-RestartStyleHint-to-RestartIfRunning-when.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# http://bugzilla.gnome.org/show_bug.cgi?id=593355
-zcat ${SB_PATCHDIR}/0001-bell-increase-bell-rate-limit-from-1-s-to-1-100ms.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# http://bugzilla.gnome.org/show_bug.cgi?id=593356
-zcat ${SB_PATCHDIR}/0001-sound-ask-libcanberra-to-cache-alert-desktop-switch-.patch.gz | patch -p1 -E --backup --verbose || exit 1
-# http://bugzilla.gnome.org/show_bug.cgi?id=593358
-zcat ${SB_PATCHDIR}/0001-tooltip-set-window-type-hint-for-self-drawn-tooltips.patch.gz | patch -p1 -E --backup --verbose || exit 1

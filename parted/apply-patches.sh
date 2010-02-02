3455cabae3099d54e531a424b7bc04e322011251
Phantom X <megaphantomx@bol.com.br>
2010-02-02 04:15:39 -0200

    eigen2, gnome-mag, highlight, mercurial, parted, pyxdg, stunnel, subversion, xmessage

--- parted/apply-patches.sh
+++ parted/apply-patches.sh
@@ -2,29 +2,6 @@
 
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/parted.configure.diff.gz | patch -p0 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-appletv-support.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-extended-mbr.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-noheaders.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-pop-push-error.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-remove-struct-elem.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-move-function-declarations.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-handle-dup-error.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-swap-flag.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-volkeysize.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-no-BLKPG.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-commit-without-close.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-dont-touch-part-nodes.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-ped_partition_is_busy-no-exception.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-gpt-big-endian.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-export-alignment-info.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-dasd-fixes.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-dasd-533808.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-lo-as-file-rh546622.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-partition-limits-rh533417.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-diskflags.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-ui-align-rh361951.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-align-default-rh361951.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-entiredisk-rh533328.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-ru-rh543029.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/parted-1.9.0-new-lvm-rh525095.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/parted-2.1-blkid_topology_get_physical_sector_size.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/parted-2.1-mem-leak-fixes-rh556012.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/parted-2.1-default-alignment.patch.gz | patch -p1 -E --backup --verbose || exit 1
--- stunnel/apply-patches.sh
+++ stunnel/apply-patches.sh
@@ -1,4 +1,4 @@
   
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/${NAME}-4.27-sample.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/${NAME}-4.30-sample.patch.gz | patch -p1 -E --backup --verbose || exit 1

87a9635b1e52577b97c89633a84df2998cf5a4a2
Phantom X <megaphantomx@bol.com.br>
2010-02-28 03:01:29 -0300

    gmerlin, gmerlin-avdecoder, lame, libxnvctrl, lives, menu-cache, moreutils, nvidia-glx, nvidia-settings, scite, violetland

--- consolekit/apply-patches.sh
+++ consolekit/apply-patches.sh
@@ -1,4 +1,9 @@
-  
+
+set -e -o pipefail
+
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/nodaemon.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/reorder-initialization.patch.gz | patch -p1 -E --backup --verbose || exit 1
+
+set +e +o pipefail
--- lame/apply-patches.sh
+++ lame/apply-patches.sh
@@ -9,6 +9,3 @@ zcat ${SB_PATCHDIR}/${NAME}-3.98-shared-frontend.patch.gz | patch -p1 -E --backu
 
 # Patch gtk stuff, otherwise eautoreconf dies
 zcat ${SB_PATCHDIR}/${NAME}-3.98-gtk-path.patch.gz | patch -p0 -E --backup --verbose || exit 1
-
-# Fix for ffmpeg-0.5, bug 265830
-zcat ${SB_PATCHDIR}/${NAME}-3.98.2-ffmpeg-0.5.patch.gz | patch -p1 -E --backup --verbose || exit 1

af2ed69d6fe0f35e78d85d957994c60193a6c7e8
Phantom X <megaphantomx@bol.com.br>
2010-03-14 04:19:00 -0300

    2mandvd, apr, exaile, libmicrohttpd, liboil, qmc2, python-mutagen, tucan

--- kde/kdemultimedia/apply-patches.sh
+++ kde/kdemultimedia/apply-patches.sh
@@ -8,7 +8,7 @@ zcat ${SB_PATCHDIR}/${NAME}-4.3.75-kscd_doc.patch.gz | patch -p1 -E --backup --v
 # git clone git://colin.guthr.ie/kdemultimedia
 # git diff master..remotes/origin/pulse > kmix_pa-<date>.patch
 if [ "${SB_PA}" = "YES" ] ;then
-  zcat ${SB_PATCHDIR}/kmix_pa-20100129.patch.gz | patch -p1 -E --backup --verbose
+  zcat ${SB_PATCHDIR}/kmix-pulse.patch.gz | patch -p1 -E --backup --verbose
 fi
 
 set +e +o pipefail
--- kde/phonon/apply-patches.sh
+++ kde/phonon/apply-patches.sh
@@ -15,6 +15,7 @@ fi
 zcat ${SB_PATCHDIR}/${NAME}-4.3.50-fix-decodebin-usage.patch.gz | patch -p0 -E --backup --verbose
 zcat ${SB_PATCHDIR}/${NAME}-4.3.50-gstreamer-fix-seekable-query-failed.patch.gz | patch -p1 -E --backup --verbose
 zcat ${SB_PATCHDIR}/${NAME}-4.3.50-phonon-allow-stop-empty-source.patch.gz | patch -p1 -E --backup --verbose
+zcat ${SB_PATCHDIR}/${NAME}-4.3.80-pulse-devicemove-rejig.patch.gz | patch -p1 -E --backup --verbose
 # https://bugs.kde.org/213710
 zcat ${SB_PATCHDIR}/${NAME}-4.4.0-subtitles-files-phonon-xine.patch.gz | patch -p1 -E --backup -z subtitles --verbose
 

15b9b9a2a40b25ad3a22cc6dbe9dddc6529cb58c
Phantom X <megaphantomx@bol.com.br>
2010-04-04 05:05:06 -0300

    compiz, compiz-plugins-{extra,main}, dos2unix, epic4, konqinfohelper, libdrm, machine-update, tigervnc, xf86-video-fbdev

--- xorg/compiz/apply-patches.sh
+++ xorg/compiz/apply-patches.sh
@@ -1,7 +1,9 @@
-  
+
+set -e -o pipefail
+
 SB_PATCHDIR=${CWD}/patches
 
-zcat ${SB_PATCHDIR}/composite-cube-logo.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/${NAME}-0.8.2-wall.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/unknown-key.patch.gz | patch -p1 -E --backup --verbose || exit 1
-zcat ${SB_PATCHDIR}/kde44-api.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/composite-cube-logo.patch.gz | patch -p1 -E --backup --verbose
+zcat ${SB_PATCHDIR}/${NAME}-0.8.2-wall.patch.gz | patch -p1 -E --backup --verbose
+
+set +e +o pipefail

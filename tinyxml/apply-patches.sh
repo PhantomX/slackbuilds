5b038ea4aea9678ab10ee54c61af58aaebce5286
Phantom X <megaphantomx@bol.com.br>
2010-07-05 03:23:40 -0300

    bitlbee, cryptsetup, hydrogen, mkvtoolnix, portmidi, ruby, tinyxml

--- tinyxml/apply-patches.sh
+++ tinyxml/apply-patches.sh
@@ -4,5 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+zcat ${SB_PATCHDIR}/${NAME}-2.5.3-stl.patch.gz | patch -p1 -E --backup -z .stl --verbose
 
 set +e +o pipefail

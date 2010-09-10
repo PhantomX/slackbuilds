dc1a7c9434a66cc4c01ab243fd7a4b93abd4a46e
Phantom X <megaphantomx@bol.com.br>
2010-09-09 22:25:52 -0300

    alac_decoder, drumstick, firefox, googleearth, hunspell-pt, kde-plasma-mpdnowplaying, libtorrent-rasterbar, libvdpau, nvidia-glx, phoronix-test-suite, polkit-qt, seamonkey, xf86-input-mouse, xulrunner

--- kde/konq-plugins/apply-patches.sh
+++ kde/konq-plugins/apply-patches.sh
@@ -6,5 +6,8 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 # fix build with KDE 4.3.80's version of webkitkde (upstream patch)
 zcat ${SB_PATCHDIR}/${NAME}-4.4.0-babelfish-kwebkitpart.patch.gz | patch -p0 -E --backup --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.4.0-qt47.patch
+# http://bugs.kde.org/234624
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.4.0-kde234624.patch
 
 set +e +o pipefail
--- libtorrent-rasterbar/apply-patches.sh
+++ libtorrent-rasterbar/apply-patches.sh
@@ -5,6 +5,5 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.dpatch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/disable-valloc.dpatch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/rb_libtorrent-0.14.10-gcc45.patch
 
 set +e +o pipefail

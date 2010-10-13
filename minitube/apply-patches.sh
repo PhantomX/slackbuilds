85ec2efa7f5df7419c61c392e5ecd48dda019177
Phantom X <megaphantomx@bol.com.br>
2010-10-12 23:46:44 -0300

    aria2, clementine, digikam, eigen2, fluidsynth, gtk-murrine-engine, kipi-plugins, libgpod, lxterminal, minitube, ntfs-3g, virtualbox, wireshark, xdotool

--- minitube/apply-patches.sh
+++ minitube/apply-patches.sh
@@ -5,5 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p0 -E --backup -z .config --verbose -i ${SB_PATCHDIR}/${NAME}-1.1-config.patch
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.2-system-qtsingleapplication.patch
 
 set +e +o pipefail

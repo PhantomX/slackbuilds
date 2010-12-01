bff3cca36a6e5413a5471f2316aca96b1e747011
Phantom X <megaphantomx@bol.com.br>
2010-11-30 23:45:39 -0200

    emelfm2, hyphen, libgphoto2, libsoup, qca, qjackctl, subversion, vim, xterm

--- libgphoto2/apply-patches.sh
+++ libgphoto2/apply-patches.sh
@@ -9,6 +9,5 @@ zcat ${SB_PATCHDIR}/libgphoto2-storage.patch.gz | patch -p1 -E --backup --verbos
 zcat ${SB_PATCHDIR}/libgphoto2-ixany.patch.gz | patch -p1 -E --backup --verbose
 zcat ${SB_PATCHDIR}/libgphoto2-maxentries.patch.gz | patch -p1 -E --backup --verbose
 zcat ${SB_PATCHDIR}/libgphoto2-device-return.patch.gz | patch -p1 -E --backup --verbose
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/libgphoto2-st2205-compile.patch
 
 set +e +o pipefail

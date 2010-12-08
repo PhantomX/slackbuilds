a19e6b9dae2b2869de9bc9e0b1395b631e24bc48
Phantom X <megaphantomx@bol.com.br>
2010-12-07 23:22:20 -0200

    adobereader, audacious, audacious-plugin, bashrun, chromium, fwbuilder, geany, git, icu, knemo, libfwbuilder, libtasn1, lvm2, qbittorrent, stellarion, webkitgtk, xbindkeys

--- icu/apply-patches.sh
+++ icu/apply-patches.sh
@@ -4,11 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-zcat ${SB_PATCHDIR}/icu-3.4-multiarchdevel.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/icu.6995.kannada.patch.gz | patch -p1 -E --backup --verbose
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/icu.7971.buildfix.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/icu.7972.buildfix.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/icu.7932.doublecompare.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/icu.8011.buildfix.patch
 
 set +e +o pipefail

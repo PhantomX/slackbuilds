91e7a78c230c9c336e38170de3950038c26e4bd2
Phantom X <megaphantomx@bol.com.br>
2011-03-15 22:07:40 -0300

    bash, binutils, ffmpeg, glib2, gnome-themes-standard, gnucash, gtk+2, gtk+3, icu, ktorrent, libktorrent, kernel-custom, libdvdnav, midori, mount-gtk, mplayer*, openshot, phoronix-test-suite, picard, python-httplib2, roxterm, x264, util-macros, xterm

--- kernel-kmod/kmod-vhba-custom/apply-patches.sh
+++ kernel-kmod/kmod-vhba-custom/apply-patches.sh
@@ -4,6 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 #patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/vhba-20101015-scsi-host-lock-push-down.patch
 
 set +e +o pipefail

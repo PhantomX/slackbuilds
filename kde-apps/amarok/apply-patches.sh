6f87d5259ee636f05482e806050604db4171704e
Phantom X <megaphantomx@bol.com.br>
2011-08-02 23:03:11 -0300

    acpid, amarok, bluez, e16, libreoffice, libtorrent-rasterbar, mesa, mozilla-firetray, nvidia-glx, picard, purple-msn-pecan, stunnel, teeworlds

--- kde-apps/amarok/apply-patches.sh
+++ kde-apps/amarok/apply-patches.sh
@@ -5,7 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 #zcat ${SB_PATCHDIR}/amarok-2.2.1.90-qtscript_not_required.patch.gz | patch -p1 -E --backup --verbose
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.4.1-ffmpeg.patch
 
 ## upstream patches
 
--- teeworlds/apply-patches.sh
+++ teeworlds/apply-patches.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup -z .extlibs --verbose -i ${SB_PATCHDIR}/${NAME}-0.6.0-extlibs.patch
+patch -p1 -E --backup -z .extlibs --verbose -i ${SB_PATCHDIR}/${NAME}-0.6.1-extlibs.patch
 patch -p1 -E --backup -z .optflags --verbose -i ${SB_PATCHDIR}/${NAME}-0.6.0-optflags.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.6.0-cstddef.patch
 

6ee875a7ef1df6f1a3e6ba489d37d64213283a1b
Phantom X <megaphantomx@bol.com.br>
2011-08-09 23:38:57 -0300

    cifs-utils, gnutls, gstreamermm, libburnia, libogg, p11-kit, rednotebook, ruby-gtk2, samba, vamp-plugin-sdk, wget

--- shadow/apply-patches.sh
+++ shadow/apply-patches.sh
@@ -6,7 +6,6 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/${NAME}-shadowconfig-man.patch.gz | patch -p1 --verbose --backup
 zcat ${SB_PATCHDIR}/${NAME}.login.defs.diff.gz | patch -p0 --verbose --backup
-zcat ${SB_PATCHDIR}/${NAME}-4.1.4.2-redhat1.patch.gz | patch -p1 --verbose --backup
 if [ "${SB_RH}" = "YES" ]; then
   zcat ${SB_PATCHDIR}/${NAME}-4.1.4.2-redhat2.patch.gz | patch -p1 --verbose --backup
   patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-uflg.patch
@@ -16,11 +15,13 @@ patch -p1 --verbose --backup -i ${SB_PATCHDIR}/${NAME}-4.1.4.3-goodname.patch
 zcat ${SB_PATCHDIR}/${NAME}-4.1.4.2-leak.patch.gz | patch -p1 --verbose --backup
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-fixes.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-infoParentDir.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-semange.patch
+#patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.3-semange.patch
 #patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-acl.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-underflow.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.2-gshadow.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.3-nopam.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.3-IDs.patch
+#patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-4.1.4.3-man.patch
 
 for patch in ${SB_PATCHDIR}/r3*.diff ;do
   patch -p0 -i ${patch}
--- wget/apply-patches.sh
+++ wget/apply-patches.sh
@@ -4,5 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+patch -p1 -E --backup -z .path --verbose -i ${SB_PATCHDIR}/wget-1.13-path.patch
 
 set +e +o pipefail

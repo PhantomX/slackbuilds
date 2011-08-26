ac976a75e347df22404a823b208c7d96d7184318
Phantom X <megaphantomx@bol.com.br>
2011-08-25 22:49:59 -0300

    audacious, audacious-plugins, git, libffi, pkcs11-helper, python-slip, rasql, system-config-samba, system-config-services, systemd, traceroute, xterm

--- libffi/apply-patches.sh
+++ libffi/apply-patches.sh
@@ -4,6 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/libffi-3.0.9-defines.patch
 
 set +e +o pipefail
--- xterm/apply-patches.sh
+++ xterm/apply-patches.sh
@@ -5,6 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/${NAME}-242-resources.patch.gz | patch -p1 -E --backup --verbose
-patch -p1 -E --backup -z .man-page_paths --verbose -i ${SB_PATCHDIR}/${NAME}-256-man-page_paths-slk.patch
+patch -p1 -E --backup -z .man-page_paths --verbose -i ${SB_PATCHDIR}/${NAME}-272-man-page_paths.patch
 
 set +e +o pipefail

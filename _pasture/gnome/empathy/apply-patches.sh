020905b32fee305cd8620f7fa38b97994c9987c6
Phantom X <megaphantomx@bol.com.br>
2011-09-14 02:49:32 -0300

    bzr, curl, dconf, gnome-common, gnome-keyring, httpd, libmicrohttpd, mc, qbittorrent, quassel, scons, violentland, vte3, xfdesktop

--- _pasture/gnome/empathy/apply-patches.sh
+++ _pasture/gnome/empathy/apply-patches.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.32.2-libnotify-0.7.patch
+#patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.32.2-libnotify-0.7.patch
 
 set +e +o pipefail
--- desktop-file-utils/apply-patches.sh
+++ desktop-file-utils/apply-patches.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.32.0-libnotify-0.7.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.18-unity.patch
 
 set +e +o pipefail

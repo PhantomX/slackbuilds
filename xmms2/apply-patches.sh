b6e64191abd7929a57b6477a8dbdadff0a401654
Phantom X <megaphantomx@bol.com.br>
2011-10-25 23:04:51 -0200

    aqbanking, dvdstyler, git, gwenhywfaf, leptonica, libmtp, plowshare, smb4k, soprano, stunnel, subversion, telepathy-glib, tesseract-ocr, xmms2, xmoto

--- xmms2/apply-patches.sh
+++ xmms2/apply-patches.sh
@@ -4,18 +4,10 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-# Use libdir properly
-zcat ${SB_PATCHDIR}/xmms2-0.7-use-libdir.patch.gz | patch -p1 -E --backup -z .libdir --verbose
 
-if [ "${SB_PA}" = "YES" ] ;then
-  # Set default output to pulse
-  zcat ${SB_PATCHDIR}/xmms2-0.6DrMattDestruction-pulse-output-default.patch.gz | patch -p1 -E --backup --verbose
-fi
-
-# Don't add extra CFLAGS, we're smart enough, thanks.
-zcat ${SB_PATCHDIR}/xmms2-0.7-no-O0.patch.gz | patch -p1 -E --backup --verbose
+# Set default output to pulse
+[ "${SB_PA}" = "YES" ] && patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/xmms2-0.8-pulse-output-default.patch
 # More sane versioning
-zcat ${SB_PATCHDIR}/xmms2-0.7-moresaneversioning.patch.gz | patch -p1 -E --backup --verbose
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/xmms2-0.7-ffmpeg.patch
+patch -p1 -E --backup -z .versionsanity --verbose -i ${SB_PATCHDIR}/xmms2-0.8-moresaneversioning.patch
 
 set +e +o pipefail

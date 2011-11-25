f759f5414ac80bb5b88217c5c30614a52ccfe588
Phantom X <megaphantomx@bol.com.br>
2011-11-24 22:56:42 -0200

    bash, binutils, gdbm, john, links, papyon, readline,

--- gdbm/apply-patches.sh
+++ gdbm/apply-patches.sh
@@ -4,7 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/gdbm-1.9.1-zeroheaders.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/gdbm-1.9.1-shortread.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/gdbm-1.10-zeroheaders.patch
 
 set +e +o pipefail
--- links/apply-patches.sh
+++ links/apply-patches.sh
@@ -5,6 +5,5 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup -z .ssl-verify --verbose -i ${SB_PATCHDIR}/links-2.3pre2-ssl-verify.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/links-2.3_pre1-libpng-1.5.patch
 
 set +e +o pipefail

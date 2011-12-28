e715760db1d69e330fd69c7498e4aabadbe6718f
Phantom X <megaphantomx@bol.com.br>
2011-12-27 23:26:42 -0200

    backlite, claws-mail, claws-mail-plugins, k9copy, libecap, libexosip2, libosip2, linphone, mcomix, mediastreamer, ortp, privoxy, source-highlight

--- claws-mail/apply-patches.sh
+++ claws-mail/apply-patches.sh
@@ -5,13 +5,5 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/11mark_trashed_as_read.patch.gz | patch -p1 -E --backup --verbose
-# http://www.thewildbeast.co.uk/claws-mail/bugzilla/attachment.cgi?id=960
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/claws-gnome-shell-dont-start-minimized.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs8
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs10
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs11
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs15
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs16
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/3.7.10cvs18
 
 set +e +o pipefail
--- ortp/apply-patches.sh
+++ ortp/apply-patches.sh
@@ -4,7 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/ortp-0.16.1-retval.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/ortp-0.16.5-unusedvar.patch
 
 set +e +o pipefail

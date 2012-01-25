3740cac84e6cebfa9156f73a3b90d074498f0123
Phantom X <megaphantomx@bol.com.br>
2012-01-24 23:03:44 -0200

    akonadi, chromaprint, clementine, curl, lftp, ntfs-3g_ntfsprogs, opera, qmmp, rsyslog, xaw3d -> libxaw3d

--- rsyslog/apply-patches.sh
+++ rsyslog/apply-patches.sh
@@ -5,5 +5,7 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-5.8.6-systemd-slk.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-5.8.7-sysklogd-compat-1-template.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-5.8.7-sysklogd-compat-2-option.patch
 
 set +e +o pipefail

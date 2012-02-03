283ba6e775f352cc70423d234a0d9ee0cde83a42
Phantom X <megaphantomx@bol.com.br>
2012-02-02 23:28:11 -0200

    firefox, ksysctl, libpng, mercurial, orage, xulrunner

--- firefox/apply-patches.sh
+++ firefox/apply-patches.sh
@@ -8,16 +8,19 @@ SB_PATCHDIR=${CWD}/patches
 patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-install-dir.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-5.0-asciidel.patch
 patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-8.0-enable-addons.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mozilla-682832-proxy.patch
+patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/mozilla-682832-proxy.patch
+patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/mozilla-722127.patch
 
 # Upstream patches
 
 # OpenSuse kde integration support
-install -m 644 ${SB_PATCHDIR}/kde.js browser/app/profile/kde.js
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-cross-desktop.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-kde.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-browser-css.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mozilla-nongnome-proxies.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mozilla-kde.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-browser-css.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/firefox-cross-desktop.patch
+if [ "${SB_KDE}" = "YES" ] ;then
+  install -m 644 ${SB_PATCHDIR}/kde.js browser/app/profile/kde.js
+  patch -p1 -E --backup -z .moz-kde --verbose -i ${SB_PATCHDIR}/mozilla-kde.patch
+  patch -p1 -E --backup -z .fx-kde --verbose -i ${SB_PATCHDIR}/firefox-kde.patch
+fi
 
 set +e +o pipefail

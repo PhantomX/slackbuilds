a84eec0de5de544d7a1fc2dad496a1dd03391adf
Phantom X <megaphantomx@bol.com.br>
2012-02-23 23:01:41 -0200

    clanlib, git, kvpm, lvm2, parallel, subtitleeditor, unetbootin, zsh

--- subtitleeditor/apply-patches.sh
+++ subtitleeditor/apply-patches.sh
@@ -4,6 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.39.0-temp-addr.patch
 
 set +e +o pipefail

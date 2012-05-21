7a5c64db9c512497d5e34b87928c9bf97bd10344
Phantom X <megaphantomx@bol.com.br>
2012-05-20 23:20:32 -0300

    binutils, c++-gtk-utils, doxygen, fftw, gdk-pixbuf2, getmail, libnetfilter_conntrack, libssh2, libwnck3, lilypond, live, mesa, obexd, qjackctl, sudo, synergy, transmission, xdg-utils, xorg-server

--- kde-apps/kmplayer/apply-patches.sh
+++ kde-apps/kmplayer/apply-patches.sh
@@ -4,16 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-# reinclude docs and translations
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-include-doc-po.patch
-# fix desktop file permission
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-fix-perms.patch
-# build fix against more recent glib2
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-buildfix-glib.patch
 # default to using phonon (instead of mplayer)
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.11.0a-kmplayerrc_phonon_default.patch
-# http://websvn.kde.org/?view=revision&revision=1225583
-# except the test lines are re-introduced
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-desktop_validate.patch
 
 set +e +o pipefail

e405867ab99fe9c9b7c0cebd4e7d6c1110392d0a
Phantom X <megaphantomx@bol.com.br>
2012-07-12 22:46:29 -0300

    aqbanking, claws-mail-themes, clawsker, geany-plugins, git, grisbi, gwenhywfar, libexif, libofx, libpng, libreoffice, mesa, netle, opencv, pkg-config, polkit, poppler, telepathy-qt

--- opencv/apply-patches.sh
+++ opencv/apply-patches.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${PNAME}-2.3.1-opencvconfig.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${PNAME}-2.3.1-numpy.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-pkgcmake.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-pkgcmake2.patch
 
 set +e +o pipefail
--- polkit/apply-patches.sh
+++ polkit/apply-patches.sh
@@ -5,6 +5,9 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 
+# Patch the patch
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/polkit-fix-deb-patch.patch
+
 for ignore in 01 05 06 ;do
   sed -i -e "/^${ignore}.*$/d" debian/patches/series
 done

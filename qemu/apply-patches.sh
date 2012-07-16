e5944ccfc183bbb0e90dde5ffa7fb64f68083e50
Phantom X <megaphantomx@bol.com.br>
2012-07-15 22:27:44 -0300

    c++-gtk-utils, cryptsetup, doxygen, ffmpeg, glib2, gparted, gtk+3, libvpx, live, mplayer*, pcsxr, qemu, sgabios

--- qemu/apply-patches.sh
+++ qemu/apply-patches.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/Fix_save-restore_of_in-kernel_i8259.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-kvm-Enable-use-of-kvm_irqchip_in_kernel-in-hwlib-cod.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-Replace-struct-siginfo-with-siginfo_t.patch
 
 set +e +o pipefail

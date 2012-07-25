a8bff9825a738de4e7345330a7a3ebed2875a6ac
Phantom X <megaphantomx@bol.com.br>
2012-07-24 23:03:02 -0300

    bin, dbus, python-decorator, transmission, wireshark, xf86-input-{evdev, keyboard}, zoneinfo, zsh

--- wireshark/apply-patches.sh
+++ wireshark/apply-patches.sh
@@ -8,9 +8,5 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.2.4-enable_lua.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-libtool-pie.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.6.1-group-msg.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.6.0-soname.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.6.2-nfsv41-addstatus.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-gnome3-msgbox.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-import-crash.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-netlogon-aes.patch
- 
+
 set +e +o pipefail

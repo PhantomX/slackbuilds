e4252282c3216d82ffb62a4d06e6bea7b07db6ad
Phantom X <megaphantomx@bol.com.br>
2012-07-31 22:33:08 -0300

    bison, e2fsprogs, fluxbox-chinfo-menu, git, iptables, nvidia-glx, openldap-client, osmo, pyxdg, qmmp, wine, xscreensaver

--- osmo/apply-patches.sh
+++ osmo/apply-patches.sh
@@ -4,8 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-fix-version-string.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-configure.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-aplay.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-libnotify-0.7.0.patch
 
 set +e +o pipefail
--- python-modules/pyxdg/apply-patches.sh
+++ python-modules/pyxdg/apply-patches.sh
@@ -5,7 +5,5 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/pyxdg-wine.patch
-# Remove strxfrm, crashing with unicode strings
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pyxdg-remove-strxfrm.patch
 
 set +e +o pipefail

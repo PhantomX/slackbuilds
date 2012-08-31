7c4cc9f0069430f40231684f6ee311fc2ee627ea
Phantom X <megaphantomx@bol.com.br>
2012-08-30 22:48:43 -0300

    calligra, gajim, libreoffice, opera, seamonkey, upower, wesnoth, xfe

--- seamonkey/apply-patches.sh
+++ seamonkey/apply-patches.sh
@@ -5,7 +5,7 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -d mozilla/gfx/cairo/cairo -i ${SB_PATCHDIR}/01_fix_slowness.patch
-patch -p1 -E --backup --verbose -d mozilla -i ${SB_PATCHDIR}/xulrunner-10.0-gcc47.patch
+patch -p2 -E --backup --verbose -d mozilla -i ${SB_PATCHDIR}/xulrunner-15.0-gcc47.patch
 patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/seamonkey-8.0-enable-addons.patch
 patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/seamonkey-idl-parser-cachepath.patch
 

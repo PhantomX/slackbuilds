65babab5206d197b51783486c10ade440ae39e75
Phantom X <megaphantomx@bol.com.br>
2012-10-24 02:44:25 -0200

    at-spi2-{atk,core}, bison, cairo, coreutils, dpkg, dvdauthor, fonts-tweak-tool, giflib, git, imagemagick, inkscape, libextractor, parallel, poppler-data, spacefm, systemd, vim*, webkitgtk

--- libdmtx/apply-patches.sh
+++ libdmtx/apply-patches.sh
@@ -5,5 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.7.2-rb192.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.7.2-php54.patch
 
 set +e +o pipefail
--- libextractor/apply-patches.sh
+++ libextractor/apply-patches.sh
@@ -4,5 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+# Fix build with giflib >= 4.2.0
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/libextractor-giflib420.patch
 
 set +e +o pipefail

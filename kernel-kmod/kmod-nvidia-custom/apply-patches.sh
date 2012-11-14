5c045029d1545c41f637488ac3600bf836daeb38
Phantom X <megaphantomx@bol.com.br>
2012-11-13 22:43:22 -0200

    at-spi2-*, binutils, glib-networking, gobject-introspection, gucharmap, gvfs, irrlicht, libsoup, libenck3, lilypond, nvidia-glx, os-prober, phonon-backend-vlc, telepathy-glib, vte3

--- kernel-kmod/kmod-nvidia-custom/apply-patches.sh
+++ kernel-kmod/kmod-nvidia-custom/apply-patches.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p2 -E --backup --verbose -i ${SB_PATCHDIR}/256.35-unified-arch.patch
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/310.19-unified-arch.patch
 
 set +e +o pipefail
--- os-prober/apply-patches.sh
+++ os-prober/apply-patches.sh
@@ -10,7 +10,6 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-newnsdirfix.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-no-dummy-mach-kernel.patch
 # Sent upstream
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-mdraidfix.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-grub2-fedorafix.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-yaboot-parsefix.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-usrmovefix.patch
 

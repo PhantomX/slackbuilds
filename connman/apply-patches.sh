8622ebab18427df3132eeb82d8eb5ff678acd4f7
Phantom X <megaphantomx@bol.com.br>
2012-12-25 21:37:09 -0200

    connman, e17, feh, fribidi, gettext, serd, smplayer, spacefm, suil

---
+++ connman/apply-patches.sh
@@ -0,0 +1,9 @@
+
+set -e -o pipefail
+
+SB_PATCHDIR=${CWD}/patches
+
+# patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/allow_group_network.diff
+
+set +e +o pipefail
--- hddtemp/apply-patches.sh
+++ hddtemp/apply-patches.sh
@@ -5,7 +5,8 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/hddtemp-db.patch.gz | patch -p0 -E --backup --verbose
-# http://savannah.nongnu.org/bugs/index.php?28896
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/hddtemp-0.3-beta15-reg-eip.patch
+# https://bugzilla.redhat.com/show_bug.cgi?id=717479
+# https://bugzilla.redhat.com/show_bug.cgi?id=710055
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/hddtemp-0.3-beta15-autodetect-717479.patch
 
 set +e +o pipefail

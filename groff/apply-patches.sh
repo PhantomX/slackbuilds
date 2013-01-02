4fe8150f847d9185cee516dac57f07b6a9e68a14
Phantom X <megaphantomx@bol.com.br>
2013-01-02 01:14:10 -0200

    automake, c++-gtk-utils, fluxbox, fotoxx, git, groff, jack, libdvbpsi, opus, qx11grab, redland, rekonq, rxvt-unicode, sharutils

--- groff/apply-patches.sh
+++ groff/apply-patches.sh
@@ -4,10 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/groff-info-missing-x11.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/groff-makefile-typo.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/groff-manpages-typos.patch
-patch -p1 -E --backup -z .grofferdir-auto --verbose -i ${SB_PATCHDIR}/groff-grofferdir-auto.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/groff-grotty-wc-no-sgr.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-missing-groff-x11-info-message-when-gxditview-not-fo.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0003-various-security-fixes.patch
 
 set +e +o pipefail
--- jack/apply-patches.sh
+++ jack/apply-patches.sh
@@ -10,16 +10,13 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-audio-connection-kit-no_d
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-doxygen-output-dir-fix.patch
 # We don't want the internal API documentation
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-apidoc-only.patch
-# Enable ffado buffersize change at runtime. From upstream trunk
-# https://github.com/jackaudio/jack2/commit/96e025123
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-ffado-buffersize.patch
+# Fix doxygen doc build regression. From upstream trunk 95a1162d6aecc91
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-doxygen-buildfix.patch
+# Adjust default priority. RHBZ#795094
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-realtime-compat.patch
-# Fix jack-connect segfault when invoked with no arguments. From upstream trunk
-# https://github.com/jackaudio/jack2/commit/00280570a
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-fix-connect-segfault.patch
+# Enable renaming and reordering the jack ports RHBZ#887408
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-portnames.patch
 # Fix ppc64 mpd startup issue RHBZ#799552
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-ppc64-long.patch
-# uc_regs no longer available on ppc64
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/jack-audio-connection-kit-ppc-uc_regs.patch 
 
 set +e +o pipefail

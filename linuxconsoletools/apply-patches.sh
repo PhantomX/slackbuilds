324d808fa39567ece1cc82bdfa450aa481228e59
Phantom X <megaphantomx@bol.com.br>
2013-01-24 21:05:43 -0200

    calligra, fonts-tweak-tool, joystick->linuxconsoletools, perl-libintl, rednotebook, seamonkey, xsetroot

--- linuxconsoletools/apply-patches.sh
+++ linuxconsoletools/apply-patches.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-1.4.2-build.patch
+# From Gentoo
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/joystick-1.4.4-build.patch
 
 set +e +o pipefail

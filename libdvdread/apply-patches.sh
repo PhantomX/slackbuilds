bf23e97965fe3094fdd1b2488eec3edc97820050
Phantom X <megaphantomx@bol.com.br>
2013-04-16 22:41:13 -0300

    bison, ffmpeg, freeglut, gdk-pixbuf2, glib2, gnome-themes-standard, gtk+3, gucharmap, libdvdnav, libdvdread, libvpx, mplayer*, qpdf, rtmdump, seamonkey, vte3, wayland, weston, zeitgeist

--- libdvdread/apply-patches.sh
+++ libdvdread/apply-patches.sh
@@ -6,5 +6,6 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup -z .multilib --verbose -i ${SB_PATCHDIR}/libdvdread-multilib.patch
 zcat ${SB_PATCHDIR}/04-md5-symbols.patch.gz | patch -p1 -E --backup --verbose
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/libdvdread-deprecated.patch
 
 set +e +o pipefail
--- mplayer/apply-patches.sh
+++ mplayer/apply-patches.sh
@@ -6,7 +6,7 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/${NAME}-1.0pre8-config.patch.gz | patch -p0 -E --backup --verbose
 zcat ${SB_PATCHDIR}/${NAME}-ivtv.patch.gz | patch -p0 -E --backup --verbose
-zcat ${SB_PATCHDIR}/${NAME}-fix-mkv.patch.gz | patch -p0 -E --backup --verbose
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-fix-mkv.patch
 # http://svnweb.freebsd.org/ports/head/multimedia/mplayer/files
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/mplayer-PrintGifError.patch
 

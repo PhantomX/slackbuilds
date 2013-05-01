eea2b0789169275bff45b29c0429ce7bb8f8d338
Phantom X <megaphantomx@bol.com.br>
2013-05-01 02:51:37 -0300

    2mandvd, e16, fvwm-crystal, glibmm, gparted, libebml, libmatroska, linux-firmware, mesa, mkvtoolnix, youtube-dl, zlib

--- 2mandvd/apply-patches.sh
+++ 2mandvd/apply-patches.sh
@@ -4,7 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-fix-const-char-concatenation.patch
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-ffmpeg.patch
 
 set +e +o pipefail
--- clearlooks-phenix-theme/apply-patches.sh
+++ clearlooks-phenix-theme/apply-patches.sh
@@ -4,5 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+# Fix strange padding with gtk-3.0 theme.
+patch -p0 -E --verbose -i ${SB_PATCHDIR}/${NAME}-padding.patch
 
 set +e +o pipefail

f23ec9768b909d4e9adc46d93d048a1d7be19c1f
Phantom X <megaphantomx@bol.com.br>
2013-05-14 19:50:11 -0300

    cd-discid, colord, glib2, gtk+2, gtk+3, gucharmap, libgee, pango, pygobject3, skrooge

--- glib2/apply-patches.sh
+++ glib2/apply-patches.sh
@@ -7,8 +7,5 @@ SB_PATCHDIR=${CWD}/patches
 # From Debian
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/04_homedir_env.patch
 patch -p1 -R -E --backup --verbose -i ${SB_PATCHDIR}/revert-warn-glib-compile-schemas.patch
-# Upstream fixes from 2.36 branch (Borrowed from Arch)
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/gvariant-fix-annotation.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/partially-revert-ce0022933c255313e010b27f977f4ae02aad1e7e.patch
 
 set +e +o pipefail
--- python-modules/python-setuptools/apply-patches.sh
+++ python-modules/python-setuptools/apply-patches.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-zcat ${SB_PATCHDIR}/setuptools-0.6_rc7-noexe.patch.gz | patch -p0 -E --backup -z .orig --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/distribute-skip-sdist_with_utf8_encoded_filename.patch
 
 set +e +o pipefail

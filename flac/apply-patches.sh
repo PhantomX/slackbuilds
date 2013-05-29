2a26d1d16aab3b0714faff2d46d08edd827c92ba
Phantom X <megaphantomx@bol.com.br>
2013-05-28 22:45:09 -0300

    ca-certificates, coloro-kde, filezilla, flac, evince, kwebkitpart, lm_sensors, pekwm, pulseaudio, qpdfview, util-linux, xterm, libdmx, libFS

--- flac/apply-patches.sh
+++ flac/apply-patches.sh
@@ -4,14 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-zcat ${SB_PATCHDIR}/flac-1.2.1-asm.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/flac-1.2.1-gcc43.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/flac-1.2.1-cflags.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/flac-1.2.1-hidesyms.patch.gz | patch -p1 -E --backup --verbose
-# reduce number of tests
-zcat ${SB_PATCHDIR}/flac-1.2.1-tests.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/flac-1.2.1-bitreader.patch.gz | patch -p0 -E --backup --verbose
-zcat ${SB_PATCHDIR}/flac.man.diff.gz | patch -p1 -E --backup --verbose
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES

9ac059b82c054f27ba4c58cc7495b268f1e0f4a1
Phantom X <megaphantomx@bol.com.br>
2013-07-11 22:49:46 -0300

    aegisub, flash-player, gthumb, libtorrent-rasterbar, liferea, mc, nvidia-glx, p11-kit, qbittorrent, qpdf, qt-creator, sg3_utils, soprano, squid, xombrero, xpad, xterm, zeitgeist, zoneinfo

--- fake-aaa_elflibs.sh
+++ fake-aaa_elflibs.sh
@@ -187,7 +187,7 @@ case $1 in
   *)
     txt
     exit 0
-;;
+  ;;
 esac
 
 while [ -n "$1" ] ;do
--- xpad/apply-patches.sh
+++ xpad/apply-patches.sh
@@ -4,6 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/xpad-4.0-remove-GTK_WINDOW_TOPLEVEL.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/xpad-4.1-glibc.patch
 
 set +e +o pipefail

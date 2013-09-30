f7e93a901577664580ae027efe1016cb79b4f06c
Phantom X <megaphantomx@bol.com.br>
2013-09-30 00:42:59 -0300

    dialog, gvfs, lunzip, lziprecover, neon, plzip, rsync, wine, wine_gecko

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -13,8 +13,13 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-random-seed-we-should-ret
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-core-cgroup-first-print-then-free.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0002-swap-fix-reverse-dependencies.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-cgroup-add-missing-equals-for-BlockIOWeight.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-move-utf8-functions-from-libudev-private.h-to-utf8.h.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0002-Use-udev_encode_string-in-fstab_node_to_udev_node.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-nspawn-be-less-liberal-about-creating-bind-mount-des.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-swap-create-.wants-symlink-to-auto-swap-devices.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-udev-rules-avoid-erroring-on-trailing-whitespace.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-polkit-Avoid-race-condition-in-scraping-proc.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-Fix-buffer-overrun-when-enumerating-files.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=NO
--- wine/apply-patches.sh
+++ wine/apply-patches.sh
@@ -6,16 +6,14 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/wine-1.1.15-winegcc.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0003-winemenubuilder-silence-an-err.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/wine-1.7.2-osmesa-check.patch
 
 # Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/wine-cjk.patch
 
 # winepulse backend
 # http://repo.or.cz/w/wine/multimedia.git
-filterdiff -p1 -x configure -z ${SB_PATCHDIR}/wine-pulse-1.7.0.patch | patch -p1 -E --backup --verbose
-# use winealsa for midi in the pa backend
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/wine-pulse-winmm-Load-winealsa-if-winepulse-is-found.patch
+filterdiff -p1 -x configure -z ${SB_PATCHDIR}/wine-pulse-1.7.3.patch | patch -p1 -E --backup --verbose
+
 # bugfixes
 # http://bugs.winehq.org/show_bug.cgi?id=7698
 # http://bugs2.winehq.org/attachment.cgi?id=6853

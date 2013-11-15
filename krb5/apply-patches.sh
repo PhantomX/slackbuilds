3e93f748d216f431fa3ed1cdb43f78a6adf7571b
Phantom X <megaphantomx@bol.com.br>
2013-11-15 03:30:20 -0200

    asciidoc, krb5, lvm2, mesa, mpd, nspr, nss, php, xscreensaver

--- krb5/apply-patches.sh
+++ krb5/apply-patches.sh
@@ -4,7 +4,15 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.10-ksu-access.patch
+### Fedora
+# Patches for kernel-persistent-keyring support (backport)
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/persistent_keyring.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-master-kinit-cccol.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-keyring-strtol.patch
+
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-pam.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-selinux-label.patch
+
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.10-ksu-path.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.7-ktany.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.10-buildconf.patch
@@ -14,8 +22,6 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.3.4-send-pr-tempfile.pa
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.8-api.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.10-doublelog.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.10-kpasswd_tcp.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-pam.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-selinux-label.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-dirsrv-accountlock.patch
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.9-debuginfo.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-kvno-230379.patch
@@ -28,19 +34,17 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.2-skew1.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.2-skew2.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-master-test_gss_no_udp.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-master-test_no_pmap.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-master-init_referral.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.3-skew3.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-gss-methods1.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-gss-methods2.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-kpasswdtest.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11-check_transited.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.3-prompter1.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.3-prompter2.patch
 
 # Patches for otp plugin backport
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.2-keycheck.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/krb5-1.11.2-otp.patch
-
-# Patches for kernel-persistent-keyring support (backport)
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/persistent_keyring.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES
--- linuxdoc/asciidoc/apply-patches.sh
+++ linuxdoc/asciidoc/apply-patches.sh
@@ -4,7 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-# http://groups.google.com/group/asciidoc/browse_thread/thread/7f7a633c5b11ddc3
-patch -p1 -E --backup -z .datadir --verbose -i ${SB_PATCHDIR}/${NAME}-8.6.3-datadir.patch
 
 set +e +o pipefail

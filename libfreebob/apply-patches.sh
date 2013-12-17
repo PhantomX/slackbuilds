37dbe445bbdae1de4cd8e30630b1ddb60352cb84
Tiago Brait <tiagobrait@gmail.com>
2013-12-17 02:31:24 -0200

    Fix libfreebob SlackBuild in Slackware 14.1
    libfreebob wasn't building in Slackware 14.1. Added some patches to
    make it work:
    
    * new patch to compile with gcc versions > 4.5
    * addeed a missing include in includes.patch

--- libfreebob/apply-patches.sh
+++ libfreebob/apply-patches.sh
@@ -2,3 +2,4 @@
 SB_PATCHDIR=${CWD}/patches
 
 zcat ${SB_PATCHDIR}/libfreebob-1.0.11-includes.patch.gz | patch -p1 -E --backup --verbose || exit 1
+zcat ${SB_PATCHDIR}/libfreebob-1.0.11-gcc45.patch.gz | patch -p1 -E --backup --verbose || exit 1

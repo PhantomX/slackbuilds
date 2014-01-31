9645af65ff4c863ad25548da5155c5bdd8e35ee7
Phantom X <megaphantomx@bol.com.br>
2014-01-30 23:45:31 -0200

    cmake, curl, apache-ant, hamcrest, icedtea-web, jline, junit, openjdk, rhino, xalan-j2, xerces-j2, kernel-custom, libreoffice, qtcurve, xkeyboard-config

---
+++ java/openjdk/apply-patches.sh
@@ -0,0 +1,13 @@
+
+set -e -o pipefail
+
+SB_PATCHDIR=${CWD}/patches
+
+# patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+
+cp ${SB_PATCHDIR}/*.{diff,patch} ${SB_SROOT}/patches/
+export DISTRIBUTION_PATCHES="patches/fontconfig-paths.diff \
+  patches/openjdk7_nonreparenting-wm.diff \
+  patches/java-1.7.0-openjdk-freetype-check-fix.patch"
+
+set +e +o pipefail

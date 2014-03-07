8dbb7fefdb97a85c9c9f5dc5838c2699ab3afa80
Phantom X <megaphantomx@bol.com.br>
2014-03-06 22:30:53 -0300

    cdrtools, gnutls, icedtea-web, calligra, libburnia, libclc, libetonyek, libodfgen, mariadb, mesa, ocl-icd, opencl-headers, znc

--- kde/calligra/apply-patches.sh
+++ kde/calligra/apply-patches.sh
@@ -4,5 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.7.90-no_mpxj.patch
 
 set +e +o pipefail
--- mariadb/apply-patches.sh
+++ mariadb/apply-patches.sh
@@ -4,10 +4,10 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+### Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-errno.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-strmov.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-install-test.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-expired-certs.patchh
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-s390-tsc.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-logrotate.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-cipherspec.patch
@@ -19,6 +19,5 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-covscan-signexpr.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-covscan-stroverflow.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-config.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-ssltest.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mariadb-versioning-compat.patch
 
 set +e +o pipefail

2fdded3a6f74fb5c7a7525714da49b157ac7a506
Phantom X <megaphantomx@bol.com.br>
2014-03-13 22:33:58 -0300

    belle-sip, ca-certificates, cups-filters, efl, terminology, eigen3, kde-telepathy, libantlr3c, libreoffice, linphone, luajit, mediastreamer, ortp

--- e/efl/apply-patches.sh
+++ e/efl/apply-patches.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/efl-1.8.5-systemd209.patch
+patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/efl-1.9.1-systemd209.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES
--- linphone/apply-patches.sh
+++ linphone/apply-patches.sh
@@ -4,7 +4,5 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-# Adapted from ROSA patch
-patch -p1 -E --backup -z .exosip400 --verbose -i ${SB_PATCHDIR}/${NAME}-3.6.0-exosip-4.0.0.patch
 
 set +e +o pipefail

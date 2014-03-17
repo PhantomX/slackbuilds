e369d50189fb09c2bd07305ed23b6b2159d19bf9
Phantom X <megaphantomx@bol.com.br>
2014-03-16 21:43:51 -0300

    asunder, avidemux, clamtk, f2fs-tools, ffmpeg, flash-player, galculator, libmbim, libvpx, mplayer, mpv, nspr, phoronix-test-suite, pngcrush, pyqt4, sip, python3, rsyslog, ruby-gtk2, serf, sudo, x264, x265, libxfce4ui

--- python3/apply-patches.sh
+++ python3/apply-patches.sh
@@ -11,12 +11,11 @@ for patches in \
   05_all_regenerate_platform-specific_modules.patch \
   21_all_distutils_c++.patch \
   ;do
-  patch -p0 --backup --verbose -i ${PVER}/${patches}
+  patch -p0 --backup --verbose -i patches/${patches}
 done
 
 ### Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/Python-3.1.1-rpath.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00003-remove-mimeaudio-tests.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00111-no-static-lib.patch
 if [ "${ARCH}" = "x86_64" ]; then
   patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/python-3.3.0b1-lib64.patch
@@ -33,12 +32,10 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00141-fix-test_gc_with_COUNT_A
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/00153-fix-test_gdb-noise.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00156-gdb-autoload-safepath.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00157-uid-gid-overflows.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00177-platform-unicode.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00178-dont-duplicate-flags-in-sysconfig.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00179-dont-raise-error-on-gdb-corrupted-frames-in-backtrace.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00183-cve-2013-2099-fix-ssl-match_hostname-dos.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00184-ctypes-should-build-with-libffi-multilib-wrapper.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00185-CVE-2013-4238-hostname-check-bypass-in-SSL-module.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/00186-dont-raise-from-py_compile.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES
--- rsyslog/apply-patches.sh
+++ rsyslog/apply-patches.sh
@@ -7,7 +7,6 @@ SB_PATCHDIR=${CWD}/patches
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-7.4.1-sd-service-slk.patch
 ## Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-7.2.2-manpage-dbg-mode.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-7.4.8-bz1026804-imjournal-message-loss.patch
 
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-7.6.0-systemd209.patch
 

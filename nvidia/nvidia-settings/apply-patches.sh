5a9dce077f2edeb34cc5ed9546f72524fa95fb74
Phantom X <megaphantomx@bol.com.br>
2014-04-08 22:23:47 -0300

    cups-filters, flash-player, comic-neue-fonts, gawk, gnutls, icedtea-web, tagsoup, akonadi, libkgapi, prison, kmod, libsidplayfp, nvidia-glx, openssl, wildmidi, xine-lib, youtube-dl

--- nvidia/nvidia-settings/apply-patches.sh
+++ nvidia/nvidia-settings/apply-patches.sh
@@ -5,6 +5,7 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-shared-nvctrl.patch
+patch -p0 -E --backup -z .jansson --verbose -i ${SB_PATCHDIR}/${NAME}-system-jansson.patch
 zcat ${SB_PATCHDIR}/${NAME}-190.36-vmproto.patch.gz | patch -p0 -E --backup --verbose
 
 set +e +o pipefail

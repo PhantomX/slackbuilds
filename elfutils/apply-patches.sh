a76fda916b8b025100957c5adbd8486bcde4b8bf
Phantom X <megaphantomx@bol.com.br>
2014-05-22 22:19:44 -0300

    curl, disk-indicator, dnsmasq, elfutils, gphoto2, libgphoto2, polipo, prelink, sendmail, gccmakedep, imake

--- elfutils/apply-patches.sh
+++ elfutils/apply-patches.sh
@@ -4,8 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_EPDIR}/${PSRCARCHIVE}
-
 if [ "${SB_COMPAT}" = "YES" ] ;then
   patch -p1 -E --backup --verbose -i ${SB_EPDIR}/${PSRCARCHIVE2}
   sleep 1
@@ -15,8 +13,5 @@ if [ "${SB_COMPAT}" = "YES" ] ;then
   #autoconf -f || exit 1
 fi
 #sed -i.scanf-m -e 's/%m/%a/' src/addr2line.c tests/line2addr.c || exit 1
-
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/elfutils-0.158-mod-e_type.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/elfutils-0.158-CVE-2014-0172.patch
 
 set +e +o pipefail
--- sendmail/apply-patches.sh
+++ sendmail/apply-patches.sh
@@ -6,27 +6,27 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 zcat ${SB_PATCHDIR}/sendmail-8.14.4-makemapman.patch.gz | patch -p1 -E --backup --verbose
 zcat ${SB_PATCHDIR}/sendmail-8.14.3-smrsh_paths.patch.gz | patch -p1 -E --backup --verbose
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-pid-slk.patch
-zcat ${SB_PATCHDIR}/sendmail-8.12.7-hesiod.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/sendmail-8.12.7-manpage.patch.gz | patch -p1 -E --backup --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.9-pid-slk.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-hesiod.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-manpage.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-dynamic.patch
 zcat ${SB_PATCHDIR}/sendmail-8.13.0-cyrus.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/sendmail-8.14.4-aliases_dir.patch.gz | patch -p1 -E --backup --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-aliases_dir.patch
 zcat ${SB_PATCHDIR}/sendmail-8.13.7-vacation-slk.patch.gz | patch -p1 -E --backup --verbose
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-noversion.patch
-zcat ${SB_PATCHDIR}/sendmail-8.13.1-localdomain.patch.gz | patch -p1 -E --backup --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.9-noversion.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-localdomain.patch
 cp devtools/M4/UNIX/{,shared}library.m4
 zcat ${SB_PATCHDIR}/sendmail-8.14.3-sharedmilter.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/sendmail-8.14.4-switchfile.patch.gz | patch -p1 -E --backup --verbose
-zcat ${SB_PATCHDIR}/sendmail-8.14.3-milterfdleaks.patch.gz | patch -p1 -E --backup --verbose
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-switchfile.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/sendmail-8.14.8-milterfdleaks.patch
 zcat ${SB_PATCHDIR}/sendmail-8.14.3-ipv6-bad-helo.patch.gz | patch -p1 -E --backup --verbose
 # fix compilation with libdb5
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-8.14.4-libdb5.patch
 # silence warning about missing sasl2 config in /usr/lib*, now in /etc/sasl2
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-8.14.4-sasl2-in-etc.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-8.14.8-sasl2-in-etc.patch
 # add QoS support, patch from Philip Prindeville <philipp@fedoraproject.org>
 # upstream reserved option ID 0xe7 for testing of this new feature, #576643
-sed -e "s|\`/run/|\`/var/run/|g" ${SB_PATCHDIR}/${NAME}-8.14.7-qos.patch \
+sed -e "s|\`/run/|\`/var/run/|g" ${SB_PATCHDIR}/${NAME}-8.14.8-qos.patch \
   | patch -p1 -E --backup --verbose
 
 set +e +o pipefail

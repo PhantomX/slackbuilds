e1fe7fd5ebd26b6b46370caa163ae683b0a2d74c
Phantom X <megaphantomx@bol.com.br>
2014-05-27 22:52:14 -0300

    bash, claws-mail, cups, dd_rescue, efl, edgar, gnome-mplayer, mpg123, mutt, readline, xkerboard-config

--- mutt/apply-patches.sh
+++ mutt/apply-patches.sh
@@ -4,15 +4,11 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
+### Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.18-muttrc.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.18-manual.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-updating.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-testcert.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-cabundle.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-pophash.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-notation.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-syncdebug.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.21-tmpdir.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/mutt-1.5.23-add_debug_option.patch
 
 # From Gentoo
 for patch in ${PDIR}/mutt-gentoo-${VERSION}-r?.patch ${PDIR}/mutt-gentoo-${VERSION}-r??.patch ; do
@@ -21,7 +17,6 @@ done
 
 for patches in \
   bdb-prefix.patch \
-  interix-btowc.patch \
   dont-reveal-bbc.patch \
   ;do
   patch -p1 --backup --verbose -i ${PDIR}/${patches}

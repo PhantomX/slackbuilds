6e3a5260a8053f2abf1a97af13a943bb0cfa6533
Phantom X <megaphantomx@bol.com.br>
2014-08-10 22:55:00 -0300

    bash, dos2unix, evtest, homebank, libgit2, libidn, pcsx2, pcsxr, unrar, wine, wireshark

--- alsaequal/apply-patches.sh
+++ alsaequal/apply-patches.sh
@@ -5,5 +5,6 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/alsaequal-0.6-asneeded.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/alsaequal-0.6-eq-name.patch
 
 set +e +o pipefail
--- wireshark/apply-patches.sh
+++ wireshark/apply-patches.sh
@@ -9,27 +9,18 @@ SB_PATCHDIR=${CWD}/patches
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0001-enable-Lua-support.patch
 # Fedora-specific
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0002-Customize-permission-denied-error.patch
-# Fedora-specific
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0003-Load-correct-shared-object-name-in-python.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0004-fix-documentation-build-error.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0005-fix-string-overrun-in-plugins-profinet.patch
-# Backported from upstream. See https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8326
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0006-From-Peter-Lemenkov-via-https-bugs.wireshark.org-bug.patch
-# Backported from upstream. See also https://bugzilla.redhat.com/1007139
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0007-The-beginning-of-an-openflow-dissector.patch
 # Will be proposed upstream
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0008-adds-autoconf-macro-file.patch
-# Fedora-specific
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0009-Restore-Fedora-specific-groups.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0003-fix-string-overrun-in-plugins-profinet.patch
 # Will be proposed upstream
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0010-Add-pkgconfig-entry.patch
-# Will be proposed upstream
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0011-Install-autoconf-related-file.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0004-adds-autoconf-macro-file.patch
 # Fedora-specific
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0012-move-default-temporary-directory-to-var-tmp.patch
-# Backported to 1.10.x from the patch from this ticket:
-# https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9211
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0013-Added-basic-conversation-support.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0005-Restore-Fedora-specific-groups.patch
+# Will be proposed upstream
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0006-Add-pkgconfig-entry.patch
+# Will be proposed upstream
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0007-Install-autoconf-related-file.patch
+# Fedora-specific
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0008-move-default-temporary-directory-to-var-tmp.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES

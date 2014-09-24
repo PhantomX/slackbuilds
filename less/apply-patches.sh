808c71f65dab924ce7bf9c009d546df96581bd96
Phantom X <megaphantomx@bol.com.br>
2014-09-24 00:46:10 -0300

    antimicro, appdata-tools, appstream-glib, at-spi2-*, atk, glib2, glibmm, gloox, adwaita-icon-theme, gtk+3, gvfs, ipset, libsoup, microcode_ctl, pango, qqwing, rest, stunnel, xtrans

--- less/apply-patches.sh
+++ less/apply-patches.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-zcat ${SB_PATCHDIR}/less.sysconfdir.patch.gz | patch -p1 --verbose
+#zcat ${SB_PATCHDIR}/less.sysconfdir.patch.gz | patch -p1 --verbose
 patch -p1 -E --backup -z .Foption --verbose -i ${SB_PATCHDIR}/less-444-Foption.v2.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/less-394-search.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/less-418-fsync.patch

557cedce43cbbd77c2f9c28e42c84c725d4c6839
Phantom X <megaphantomx@bol.com.br>
2014-10-07 23:40:08 -0300

    audit, ddrescue, gtk+3, libindi, libnova, libsidplayfp, pcre, qpdfview, syslinux, wxgtk3

--- libfli/apply-patches.sh
+++ libfli/apply-patches.sh
@@ -4,9 +4,6 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/never-audit.patch
-
-# Set to YES if autogen is needed
-SB_AUTOGEN=NO
+zcat ${SB_PATCHDIR}/${NAME}-suffix.patch.gz | patch -p1 -E --backup --verbose
 
 set +e +o pipefail

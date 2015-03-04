422e6968bd6a25f4a6c830e75512cb6ecd1b92d2
Phantom X <megaphantomx@bol.com.br>
2015-03-03 23:53:39 -0300

    graphicsmagick, libvorbis, lrzip, mariadb, irpf2015, ruby, virtualbox, waf, xfce4-pulseaudio-plugin, xterm

--- graphicsmagick/apply-patches.sh
+++ graphicsmagick/apply-patches.sh
@@ -5,7 +5,5 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${PNAME}-1.3.14-perl_linkage.patch
-# https://bugzilla.redhat.com/1064098
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${PNAME}-1.3.20-CVE-2014-1947.patch
 
 set +e +o pipefail

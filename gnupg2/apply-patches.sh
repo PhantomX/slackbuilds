f8155946467fb56622907166eecbf7391d461c97
Phantom X <megaphantomx@bol.com.br>
2015-04-12 23:54:35 -0300

    clamtk, emacs, feh, gnupg2, iasl, extra-cmake-modules, kf5-{kguiaddons,kwindowsystem}, libmediainfo, libzen, mediainfo, mpg123, ncdu, pciutils, ryslog, sdl_mixer, wesnoth

--- gnupg2/apply-patches.sh
+++ gnupg2/apply-patches.sh
@@ -5,6 +5,7 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup -z .secmem --verbose -i ${SB_PATCHDIR}/gnupg-2.0.20-secmem.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/gnupg-2.1.1-ocsp-keyusage.patch
 
 ## upstream patches
 
--- pcre/apply-patches.sh
+++ pcre/apply-patches.sh
@@ -7,4 +7,16 @@ SB_PATCHDIR=${CWD}/patches
 ### Fedora
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.21-multilib.patch
 
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-bug-when-there-are-unset-groups-prior-to-ACCEPT-.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-zero-repeat-assertion-condition-bug.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Remove-computing-the-JIT-read-only-data-size-in-adva.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-memory-bug-for-S-V-H-compile.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-mutual-recursion-inside-other-groups-stack-overf.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-comment-between-subroutine-call-and-quantifier-b.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-stack-overflow-instead-of-diagnostic-for-mutual-.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-pcregrep-loop-when-K-is-used-in-a-lookbehind-ass.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-pcretest-loop-for-K-in-lookbehind-assertion.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pcre-8.36-Fix-backtracking-bug-for-C-X-in-UTF-mode.patch
+
+
 set +e +o pipefail

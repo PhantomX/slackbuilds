0be08370b41ab8ecd061ca11d47daae454f8650e
Phantom X <megaphantomx@bol.com.br>
2015-07-21 23:15:11 -0300

    gdk-pixbuf2, gtk+3, iasl, openjdk, kernel-custom, pcsc-lite, postfix, rsyslog, sudo, sweethome3d

--- pango/apply-patches.sh
+++ pango/apply-patches.sh
@@ -6,5 +6,6 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 # Put modules cache in /usr/lib${LIBDIRSUFFIX}/pango
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pango-modules-cache-dir.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/pango-overflow-fix-for-pangoft2.patch
 
 set +e +o pipefail

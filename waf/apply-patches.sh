04c3603b8089344ea1d24f3d6155439d77960e35
Phantom X <megaphantomx@bol.com.br>
2015-07-28 23:59:54 -0300

    bind, byacc, git, harfbuzz, libtalloc, libtdb, libvisio, lunzip, lzip, libziprecover, lzlib, plzip, sqlite, waf

--- waf/apply-patches.sh
+++ waf/apply-patches.sh
@@ -4,7 +4,8 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/waf-1.6.2-libdir.patch
+### Fedora
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/waf-1.8.11-libdir.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/waf-1.6.9-logo.patch
 
 set +e +o pipefail

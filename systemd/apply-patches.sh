4b01350a71008a4851e834265409848bad38a38c
Phantom X <megaphantomx@bol.com.br>
2015-08-02 23:43:15 -0300

    cups, evtest, gphoto2, iso-codes, libgphoto2, libidn, mercurial, nss-gui, opencv, oprofile, scons, systemd, tbb, unetbootin

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -29,8 +29,6 @@ patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/udev-microsoft-3000-keymap.pat
 patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/60-cdrom_id.rules.diff
 
 # Upstream
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-socket-util-library-calls-shouldn-t-log-on-their-own.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-networkd-fix-size-of-networkd-binary.patch
 
 ### Fedora
 
--- tbb/apply-patches.sh
+++ tbb/apply-patches.sh
@@ -6,6 +6,6 @@ SB_PATCHDIR=${CWD}/patches
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/tbb-3.0-cxxflags.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/tbb-4.0-mfence.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/tbb-4.0-cas.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/tbb-4.3-dont-snip-Wall.patch
 
 set +e +o pipefail

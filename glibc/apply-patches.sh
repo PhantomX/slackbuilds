c869ffd23661b09d2934e30e32fa53140a56b981
Phantom X <megaphantomx@bol.com.br>
2015-08-06 23:50:24 -0300

    bluez, firefox, glibc, graphite2, jsoncpp, libreoffice, mariadb, sip, squid

--- glibc/apply-patches.sh
+++ glibc/apply-patches.sh
@@ -25,7 +25,7 @@ ApplyPatch() {
 
 # Use old-style locale directories rather than a single (and strangely
 # formatted) /usr/lib/locale/locale-archive file:
-ApplyPatch glibc.locale.no-archive.diff.gz
+ApplyPatch glibc.locale.no-archive.diff
 # The is_IS locale is causing a strange error about the "echn" command
 # not existing.  This patch reverts is_IS to the version shipped in
 # glibc-2.5:
@@ -49,14 +49,14 @@ fi
 
   ApplyPatch 00_all_0001-disable-ldconfig-during-install.patch
   ApplyPatch 00_all_0002-workaround-crash-when-handling-signals-in-static-PIE.patch
-  ApplyPatch 00_all_0003-make-fortify-logic-checks-less-angry.patch
-  ApplyPatch 00_all_0004-Fix-localedef-segfault-when-run-under-exec-shield-Pa.patch
-  ApplyPatch 00_all_0005-reload-etc-resolv.conf-when-it-has-changed.patch
-  ApplyPatch 00_all_0006-nptl-support-thread-stacks-that-grow-up.patch
-  ApplyPatch 00_all_0007-rtld-do-not-ignore-arch-specific-CFLAGS.patch
-  ApplyPatch 00_all_0008-gentoo-support-running-tests-under-sandbox.patch
-  ApplyPatch 00_all_0009-gentoo-disable-building-in-timezone-subdir.patch
-  ApplyPatch 00_all_0016-Fix-__memcpy_chk-on-non-SSE2-CPUs.patch
+  ApplyPatch 00_all_0003-Fix-localedef-segfault-when-run-under-exec-shield-Pa.patch
+  ApplyPatch 00_all_0004-reload-etc-resolv.conf-when-it-has-changed.patch
+  ApplyPatch 00_all_0005-nptl-support-thread-stacks-that-grow-up.patch
+  ApplyPatch 00_all_0006-rtld-do-not-ignore-arch-specific-CFLAGS.patch
+  ApplyPatch 00_all_0007-gentoo-support-running-tests-under-sandbox.patch
+  ApplyPatch 00_all_0008-gentoo-disable-building-in-timezone-subdir.patch
+  ApplyPatch 00_all_0012-disable-PIE-when-checking-for-PIC-default.patch
+  ApplyPatch 00_all_0013-nptl-fix-set-but-unused-warning-w-_STACK_GROWS_UP.patch
 )
 
 ## Fedora
@@ -86,18 +86,22 @@ sed -i -e 's|/sysconfig/|/default/|g' nscd/nscd.service
 
 # http://sourceware.org/ml/libc-alpha/2012-12/msg00103.html
 ApplyPatch glibc-rh697421.patch
-ApplyPatch glibc-rh757881.patch
 ApplyPatch glibc-rh741105.patch
 # Upstream BZ 14247
 ApplyPatch glibc-rh827510.patch
-# Upstream BZ 13028
-ApplyPatch glibc-rh841787.patch
 # Upstream BZ 14185
 ApplyPatch glibc-rh819430.patch
 # Fix nscd to use permission names not constants.
 ApplyPatch glibc-rh1070416.patch
 ApplyPatch glibc-aarch64-tls-fixes.patch
 ApplyPatch glibc-aarch64-workaround-nzcv-clobber-in-tlsdesc.patch
+
+# Upstream BZ 18568
+ApplyPatch glibc-rh1238412-remove-duplicate-transliterations.patch
+ApplyPatch glibc-rh1238412-addition-and-fixes-for-translit_neutral.patch
+ApplyPatch glibc-rh1238412-update-the-translit-files-to-unicode-7.0.0.patch
+ApplyPatch glibc-rh1238412-add-translit-rules-for-da-nb-nn-sv-locales.patch
+ApplyPatch glibc-rh1238412-unicode-8.0.0-update.patch
 
 ## Mandriva
 ApplyPatch glibc-2.11.1-localedef-archive-follow-symlinks.patch 
@@ -114,41 +118,7 @@ ApplyPatch glibc-2.21-pt_BR-i18nfixes.patch
 ### Arch
 
 ### master
-ApplyPatch 0001-x86-Clean-up-__vdso_clock_gettime-variable.patch
-ApplyPatch 0001-Fix-sincos-errno-setting-bug-15467.patch
-ApplyPatch 0001-Fix-dbl-64-wordsize-64-remquo-bug-17569.patch
-ApplyPatch 0001-Filter-out-PTHREAD_MUTEX_NO_ELISION_NP-bit-in-pthrea.patch
-ApplyPatch 0001-Fix-exp2-spurious-underflows-bug-16560.patch
-ApplyPatch 0001-Fix-remquo-spurious-overflows-bug-17978.patch
-ApplyPatch 0001-Fix-sign-of-remquo-zero-remainder-in-round-downward-.patch
-ApplyPatch 0001-Fix-posix_spawn-getrlimit64-namespace-bug-17991.patch
-ApplyPatch 0001-Fix-search.h-namespace-bug-17996.patch
-ApplyPatch 0001-Fix-atan-atan2-missing-underflows-bug-15319.patch
-ApplyPatch 0002-Fix-scandir-scandirat-namespace-bug-17999.patch
-ApplyPatch 0001-Adjust-timeouts-for-some-tests-to-accommodate-slow-p.patch
-ApplyPatch 0001-Compile-gcrt1.o-with-fPIC.patch
-ApplyPatch 0001-linux-open-and-openat-ignore-mode-with-O_TMPFILE-in-.patch
-ApplyPatch 0001-Fix-x86-x86_64-scalb-qNaN-Inf-bug-16783.patch
-ApplyPatch 0002-alloca-fix-buf-interaction.patch
-ApplyPatch 0003-Fix-ldbl-128ibm-acoshl-inaccuracy-bug-18019.patch
-ApplyPatch 0001-BZ-15969-search-locale-archive-again-after-alias-exp.patch
-ApplyPatch 0002-Fix-constness-error-just-introduced-in-findlocale.patch
-ApplyPatch 0001-Fix-asin-missing-underflows-bug-16351.patch
-ApplyPatch 0001-Fix-BZ-18043-buffer-overflow-read-past-the-end-in-wo.patch
-ApplyPatch 0001-Refactor-wordexp-test.c-such-that-words-always-ends-.patch
-ApplyPatch 0002-Fix-off-by-one-which-caused-BZ-18042-and-add-a-test-.patch
-ApplyPatch 0001-Fix-BZ-18043-c4-buffer-overflow-read-past-the-end-in.patch
-ApplyPatch 0001-powerpc-Remove-HAVE_ASM_GLOBAL_DOT_NAME-define.patch
-ApplyPatch 0001-Avoid-SIGFPE-in-wordexp-BZ-18100.patch
-ApplyPatch 0001-Fix-DTV-race-assert-DTV_SURPLUS-Static-TLS-limit-and.patch
-ApplyPatch 0001-Fuller-check-for-invalid-NSID-in-_dl_open.patch
-ApplyPatch 0002-Avoid-confusing-compiler-with-dynamically-impossible.patch
-ApplyPatch 0003-Better-fix-for-setenv-.-NULL.patch
-ApplyPatch 0001-CVE-2015-1781-resolv-nss_dns-dns-host.c-buffer-overf.patch
-ApplyPatch 0001-Make-time-zone-file-parser-more-robust-BZ-17715.patch
-ApplyPatch 0001-Record-TTL-also-for-DNS-PTR-queries-bug-18513.patch
-ApplyPatch 0001-Update-headers-for-Linux-4.0-4.1-definitions.patch
-ApplyPatch 0001-Set-errno-to-ENOMEM-on-overflow-in-sbrk-bug-18592.patch
-ApplyPatch 0001-Fix-ruserok-scalability-with-large-.rhosts-file.patch
+ApplyPatch 0001-Fix-powf-close-to-1-large-bug-18647.patch
+ApplyPatch 0002-Also-check-dead-data-category-NULL.patch
 
 set +e +o pipefail

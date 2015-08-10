480c1fcad23ce167dc771d927ace50da296e3bce
Phantom X <megaphantomx@bol.com.br>
2015-08-09 22:47:32 -0300

    colordiff, f2fs-tools, adobe-source-han-sans-fonts, fribidi, gvr, highlight, librsvg, libsecret, nss, osmo, pdf2djvu, procps->procps-ng, qmmp, + xmlcopyeditor

---
+++ fonts/adobe-source-han-sans-fonts/source-han-sans-tarball.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "${tag}" ] ;then
+  echo "Need tag variable exported before!"
+  exit 1
+fi
+
+module=$(basename $0 -tarball.sh)
+snaproot="https://github.com/adobe-fonts/${module}/tags/${tag}"
+srcdir=${module}-${tag}
+
+tmp=$(mktemp -d)
+
+trap cleanup EXIT
+cleanup() {
+  set +e
+  [ -z "${tmp}" -o ! -d "${tmp}" ] || rm -rf "${tmp}"
+}
+
+unset CDPATH
+pwd=$(pwd)
+
+pushd "${tmp}"
+  svn export --depth=files --force ${snaproot} ${srcdir}
+  svn export ${snaproot}/SubsetOTF ${srcdir}/SubsetOTF
+  svn export ${snaproot}/OTC ${srcdir}/OTC
+  pushd ${srcdir}
+    rm -f */*.zip
+    find . -type d -name .svn -print0 | xargs -0r rm -rf
+  popd
+  tar -Jcf "${pwd}"/${srcdir}.tar.xz ${srcdir}
+popd >/dev/null
+
--- osmo/apply-patches.sh
+++ osmo/apply-patches.sh
@@ -5,6 +5,8 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-configure.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-0.2.10-aplay.patch
+
+# Set to YES if autogen is needed
+SB_AUTOGEN=YES
 
 set +e +o pipefail

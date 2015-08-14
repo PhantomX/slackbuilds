58ba040ef80a6ff3f1a1bf1947b98b9fde35e1fd
Phantom X <megaphantomx@bol.com.br>
2015-08-13 23:01:58 -0300

    cmake, curl, geeqie, libpcap, mednagen, mlt, pdf2djvu, qemu, seabios, texinfo

--- geeqie/apply-patches.sh
+++ geeqie/apply-patches.sh
@@ -4,28 +4,11 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-### Fedora
-# add -Wl,--as-needed
-# this touches both Makefile.in and Makefile.am as autoreconf is used
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.0-LDFLAGS.patch
-# from master branch to fix regression
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-bar_keywords.c.diff
-# in upstream bug tracker
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.0-fix-fullscreen.patch
-# bz 889897
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.0-double_generic_dialog_close.patch
-# from master
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-editors-NULL.patch
-# reported upstream
-# https://sourceforge.net/tracker/?func=detail&atid=1054680&aid=3602709&group_id=222125
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-filedata-change-notification.patch
-# reported upstream
-# https://sourceforge.net/tracker/?func=detail&aid=3603866&group_id=222125&atid=1054680
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-filedata-avoid-history-abort.patch
-# reported upstream
-# https://sourceforge.net/tracker/?func=detail&aid=3605406&group_id=222125&atid=1054682
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-large-files.patch
-# LCMS2 patch from Geeqie-devel mailing-list
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/geeqie-1.1-lcms2.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0001-Fix-typo-in-debug-messages.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0002-fix-include-statements-for-large-file-support-on-32-.patch
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0003-Add-support-for-Green-Magenta-and-Yellow-Blue-anagly.patch
+
+# Set to YES if autogen is needed
+SB_AUTOGEN=NO
 
 set +e +o pipefail

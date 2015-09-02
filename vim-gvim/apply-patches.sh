3093ec73e35d46f4e042b928c653c683b0832e5a
Phantom X <megaphantomx@bol.com.br>
2015-09-02 01:53:01 -0300

    libevdev, llvm, spideroakone, squid, vim, libvdpau, xf86-input-libinput

--- vim-gvim/apply-patches.sh
+++
@@ -1,1 +0,0 @@
-../vim/apply-patches.sh
--- vim/apply-patches.sh
+++ vim/apply-patches.sh
@@ -4,13 +4,14 @@ set -e -o pipefail
 SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
-if [ "${SB_SYNUP}" = "YES" ] && [ ! -r ${SB_PATCHDIR}/updates/vim-runtime-syntax-${SYNUPVER}.patch.gz ] ;then
+if [ "${SB_SYNUP}" = "YES" ] && [ ! -r ${CWD}/vim-runtime-syntax-${SYNUPVER}.patch.gz ] ;then
   rm -rf runtime/syntax.orig
   cp -a runtime/syntax runtime/syntax.orig
   echo "Fetching vim syntax updates from ftp.nluug.nl..."
   rsync -avzcP ftp.nluug.nl::Vim/runtime/syntax/ runtime/syntax/
+  mkdir -p ${SB_PATCHDIR}/updates
   diff -u -r --new-file runtime/syntax.orig runtime/syntax | gzip -9c \
-    > ${SB_PATCHDIR}/updates/vim-runtime-syntax-${SYNUPVER}.patch.gz || :
+    > ${CWD}/vim-runtime-syntax-${SYNUPVER}.patch.gz || :
   rm -rf runtime/syntax
   mv runtime/syntax.orig runtime/syntax
 fi
@@ -22,26 +23,6 @@ if [ "${SB_HUNSPELL}" = "YES" ] ;then
   zcat ${SB_PATCHDIR}/vim-7.0-hunspell.patch.gz | patch -p1 --verbose
 fi
 
-# For patch 7.3.780
-mv runtime/doc/eval.txt runtime/doc/eval.txt.orig
-iconv -f iso-8859-1 -t utf-8 runtime/doc/eval.txt.orig -o runtime/doc/eval.txt
-
-# For 7.4.399
-mv src/testdir/test71.ok src/testdir/test71.ok.orig
-iconv -f iso-8859-1 -t utf-8 src/testdir/test71.ok.orig -o src/testdir/test71.ok
-
-if [ "${PATCHLEVEL}" -gt 0 ] ;then
-mkdir -p patches
-cp ${SB_PATCHDIR}/updates/${SVER}.* patches/
-
-cp -f ${SB_PATCHDIR}/7.4.80{2,9} patches/
-
-( SB_PATCHDIR=patches
-  for i in $( seq -w ${PATCHLEVEL} ) $( seq 1000 ${OVERPATCHLEVEL} ) ; do
-    patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${SVER}.${i}
-  done
-)
-fi
 sed -i \
   -e '/nl \\/d' \
   -e '/nl.mo \\/d' \
@@ -49,7 +30,7 @@ sed -i \
   src/po/Makefile || exit 1
 
 if [ "${SB_SYNUP}" = "YES" ] ;then
-  zcat ${SB_PATCHDIR}/updates/vim-runtime-syntax-${SYNUPVER}.patch.gz | patch -p0 --verbose
+  zcat ${CWD}/vim-runtime-syntax-${SYNUPVER}.patch.gz | patch -p0 --verbose
 fi
 
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.4-syntax.patch
@@ -59,7 +40,7 @@ zcat ${SB_PATCHDIR}/vim-6.4-checkhl.patch.gz | patch -p1 --verbose
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.3-fstabsyntax.patch
 zcat ${SB_PATCHDIR}/vim-7.0-warning.patch.gz | patch -p1 --verbose
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.4-syncolor.patch
-zcat ${SB_PATCHDIR}/vim-7.0-specedit.patch.gz | patch -p1 --verbose
+patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.0-specedit.patch
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim72-rh514717.patch
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.4-licensemacro-1151450.patch
 patch -p1 -E --verbose -i ${SB_PATCHDIR}/vim-7.4-ssh-keywords.patch

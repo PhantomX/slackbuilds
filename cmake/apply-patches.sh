bd89b2a46d45147b90aa0f4fff11c73b2ade31be
Phantom X <megaphantomx@bol.com.br>
2015-11-12 21:19:33 -0200

    cmake, gtk+3, libmicrohttpd, libpng, ming, mupdf, ncmpcpp

--- cmake/apply-patches.sh
+++ cmake/apply-patches.sh
@@ -8,7 +8,6 @@ SB_PATCHDIR=${CWD}/patches
 patch -p1 -E --backup -z .pylibs --verbose -i ${SB_PATCHDIR}/${NAME}-2.8.10-FindPythonLibs.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-3.0.0-FindBLAS.patch
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-3.0.0-FindLAPACK.patch
-patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-2.8.12-qt5.patch
 zcat ${SB_PATCHDIR}/${NAME}-2.8.0-kde3-include.patch.gz | patch -p0 -E --backup --verbose
 # Patch to find DCMTK in Fedora (bug #720140)
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-dcmtk.patch
@@ -16,11 +15,7 @@ patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}-dcmtk.patch
 # http://public.kitware.com/Bug/view.php?id=12965
 # https://bugzilla.redhat.com/show_bug.cgi?id=822796
 patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/cmake-findruby.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/cmake.git-97ffbcd8.patch
 
 ## upstream patches
-# some post v3.3.1 tag commits
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0624-FindBoost-Add-support-for-Boost-1.59.patch
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/0640-FindPkgConfig-remove-variable-dereference.patch
 
 set +e +o pipefail

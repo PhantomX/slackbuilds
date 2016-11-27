73ffd0008ee7c1fd5dce71f484e342db5cc41840
Phantom X <megaphantomx at bol com br>
2016-11-27 21:27:36 -0200

    xorg-server

--- dbus/apply-patches.sh
+++ dbus/apply-patches.sh
@@ -5,7 +5,7 @@ SB_PATCHDIR=${CWD}/patches
 
 # patch -p0 -E --backup --verbose -i ${SB_PATCHDIR}/${NAME}.patch
 # From stock
-patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/dbus-1.4.x-allow_root_globally.diff
+patch -p1 -E --backup --verbose -i ${SB_PATCHDIR}/dbus-1.10.x-allow_root_globally.diff
 
 ### Arch
 
--- xorg/xorg-server/apply-patches.sh
+++ xorg/xorg-server/apply-patches.sh
@@ -32,41 +32,27 @@ ApplyPatch() {
 ApplyPatch x11.startwithblackscreen.diff
 
 ### Fedora
-# Submitted upstream
-ApplyPatch 0001-headers-Fix-build-errors-with-latest-glibc.patch
+# Various fixes pending upstream
+ApplyPatch 0004-glamor-restore-vfunc-handlers-on-init-failure.patch
+ApplyPatch 0005-xfree86-Remove-redundant-ServerIsNotSeat0-check-from.patch
+ApplyPatch 0006-xfree86-Make-adding-unclaimed-devices-as-GPU-devices.patch
+ApplyPatch 0007-xfree86-Try-harder-to-find-atleast-1-non-GPU-Screen.patch
+ApplyPatch 0001-Fix-segfault-if-xorg.conf.d-is-absent.patch
+ApplyPatch 0001-xwayland-Fix-use-after-free-of-cursors.patch
+ApplyPatch 0001-randr-rrCheckPixmapBounding-Do-not-substract-crtc-no.patch
+ApplyPatch 0002-randr-rrCheckPixmapBounding-do-not-shrink-the-screen.patch
 
-# This really could be done prettier.
-ApplyPatch xserver-1.4.99-ssh-isnt-local.patch
-
-# ajax needs to upstream this
-ApplyPatch xserver-1.6.99-right-of.patch
 #zcat ${SB_PATCHDIR}/xserver-1.6.99-hush-prerelease-warning.patch.gz | patch -p1 --verbose
 
 ApplyPatch 0001-Always-install-vbe-and-int10-sdk-headers.patch
 
-# submitted: http://lists.x.org/archives/xorg-devel/2013-October/037996.html
-ApplyPatch exa-only-draw-valid-trapezoids.patch
-
 # because the display-managers are not ready yet, do not upstream
 ApplyPatch 0001-Fedora-hack-Make-the-suid-root-wrapper-always-start-.patch
-
-# rhbz1203780, submitted upstream
-ApplyPatch 0001-linux-Add-linux_parse_vt_settings-and-linux_get_keep.patch
-ApplyPatch 0002-linux-Add-a-may_fail-paramter-to-linux_parse_vt_sett.patch
-ApplyPatch 0003-systemd-logind-Only-use-systemd-logind-integration-t.patch
-
-# rhbz1208992, already upstream
-ApplyPatch 0001-dix-Add-unaccelerated-valuators-to-the-ValuatorMask.patch
-ApplyPatch 0002-dix-hook-up-the-unaccelerated-valuator-masks.patch
 
 # misc
 ApplyPatch 0001-Fix-segfault-when-killing-X-with-ctrl-alt-backspace.patch
 
 ApplyPatch xserver-force-hal-disable.patch
-ApplyPatch xserver-1.7.0-systemd209.patch
-
-### Arch
-ApplyPatch v2-xserver-Fix-a-crash-with-XDMCP-error-handler.patch
 
 # Set to YES if autogen is needed
 SB_AUTOGEN=YES
